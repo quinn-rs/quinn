d2da26748fdd831c1f12e84c54ce8fdd98f45bf4
Dirkjan Ochtman <dirkjan@ochtman.nl>
2018-04-06 16:43:13 +0200

    Move TLS-related code into a separate module

--- src/lib.rs
+++ src/lib.rs
@@ -3,25 +3,10 @@ extern crate rustls;
 extern crate webpki;
 extern crate webpki_roots;
 
-use rustls::Session;
-
-//use rand::Rng;
-use std::sync::Arc;
-
-pub use self::types::Packet;
-
+mod tls;
 mod types;
 
-
 pub fn connect(server: &str) {
-    //let mut rng = rand::thread_rng();
-    let mut config = rustls::ClientConfig::new();
-    config.root_store.add_server_trust_anchors(&webpki_roots::TLS_SERVER_ROOTS);
-    config.versions = vec![rustls::ProtocolVersion::TLSv1_3];
-    let tls_config = Arc::new(config);
-    let pki_server_name = webpki::DNSNameRef::try_from_ascii_str(server).unwrap();
-    let mut tls_client = rustls::ClientSession::new(&tls_config, pki_server_name);
-    let mut buf = Vec::new();
-    println!("{:?}", tls_client.write_tls(&mut buf));
-    println!("{:?}", buf);
+    let mut client = tls::Client::new(server);
+    println!("{:?}", client.get_handshake());
 }
---
+++ src/tls.rs
@@ -0,0 +1,94 @@
+use rustls::internal::msgs::{
+    base::PayloadU16,
+    codec::Codec,
+    handshake::ClientExtension,
+    quic::{ClientTransportParameters, Parameter},
+};
+use rustls::{ClientConfig, ClientSession, ProtocolVersion, Session};
+
+use std::sync::Arc;
+
+use types::TransportParameter;
+
+use webpki::DNSNameRef;
+use webpki_roots;
+
+pub struct Client {
+    pub session: ClientSession,
+}
+
+impl Client {
+    pub fn new(hostname: &str) -> Client {
+        let mut config = ClientConfig::new();
+        config.root_store.add_server_trust_anchors(&webpki_roots::TLS_SERVER_ROOTS);
+        config.versions = vec![ProtocolVersion::TLSv1_3];
+        let tls_config = Arc::new(config);
+
+        let pki_server_name = DNSNameRef::try_from_ascii_str(hostname).unwrap();
+        Client {
+            session: ClientSession::with_handshake_exts(
+                &tls_config,
+                pki_server_name,
+                vec![ClientExtension::TransportParameters(
+                    ClientTransportParameters {
+                        initial_version: 1,
+                        parameters: encode_transport_parameters(&vec![
+                            TransportParameter::InitialMaxStreamData(131072),
+                            TransportParameter::InitialMaxData(1048576),
+                            TransportParameter::IdleTimeout(300),
+                        ]),
+                    },
+                )],
+            ),
+        }
+    }
+
+    pub fn get_handshake(&mut self) -> Vec<u8> {
+        let mut buf = Vec::new();
+        self.session.write_tls(&mut buf);
+        buf
+    }
+}
+
+fn encode_transport_parameters(params: &[TransportParameter]) -> Vec<Parameter> {
+    use self::TransportParameter::*;
+    let mut ret = Vec::new();
+    for param in params {
+        let mut bytes = Vec::new();
+        match *param {
+            InitialMaxStreamData(v) |
+            InitialMaxData(v) |
+            InitialMaxStreamIdBidi(v) |
+            InitialMaxStreamIdUni(v) => {
+                v.encode(&mut bytes);
+            },
+            IdleTimeout(v) | MaxPacketSize(v) => {
+                v.encode(&mut bytes);
+            },
+            OmitConnectionId => {},
+            StatelessResetToken(ref v) => {
+                bytes.extend_from_slice(&v);
+            },
+            AckDelayExponent(v) => {
+                v.encode(&mut bytes);
+            },
+        }
+        ret.push((tag(param), PayloadU16::new(bytes)));
+    }
+    ret
+}
+
+fn tag(param: &TransportParameter) -> u16 {
+    use self::TransportParameter::*;
+    match *param {
+        InitialMaxStreamData(_) => 0,
+        InitialMaxData(_) => 1,
+        InitialMaxStreamIdBidi(_) => 2,
+        IdleTimeout(_) => 3,
+        OmitConnectionId => 4,
+        MaxPacketSize(_) => 5,
+        StatelessResetToken(_) => 6,
+        AckDelayExponent(_) => 7,
+        InitialMaxStreamIdUni(_) => 8,
+    }
+}
