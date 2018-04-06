1b7e7a6e2dddebcf4845752f380a00f8d0d0b94f
Dirkjan Ochtman <dirkjan@ochtman.nl>
2018-04-06 16:38:32 +0200

    Add TransportParameter type

--- src/types.rs
+++ src/types.rs
@@ -56,3 +56,15 @@ pub enum Frame {
     PathResponse = 0xf,
     Stream = 0x10,
 }
+
+pub enum TransportParameter {
+    InitialMaxStreamData(u32),
+    InitialMaxData(u32),
+    InitialMaxStreamIdBidi(u32),
+    IdleTimeout(u16),
+    OmitConnectionId,
+    MaxPacketSize(u16),
+    StatelessResetToken(Vec<u8>),
+    AckDelayExponent(u8),
+    InitialMaxStreamIdUni(u32),
+}
