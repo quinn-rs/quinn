6881f19d8404f94269e9c1ebb02c74d462637fb2
Dirkjan Ochtman <dirkjan@ochtman.nl>
2018-04-17 11:28:28 +0200

    Encapsulate AEAD interface and minimize allocations

--- src/hkdf.rs
+++ src/hkdf.rs
@@ -1,39 +1,34 @@
 use bytes::{BigEndian, BufMut};
 
 use ring::{
-    aead::{AES_128_GCM, SealingKey},
+    aead::{self, AES_128_GCM},
     digest::SHA256,
     hkdf,
     hmac::SigningKey,
 };
 
-pub fn client_handshake_key(conn_id: u64) -> (SealingKey, Vec<u8>) {
-    let (key, iv) = client_handshake_key_input(conn_id);
-    (SealingKey::new(&AES_128_GCM, &key).unwrap(), iv)
-}
-
-fn client_handshake_key_input(conn_id: u64) -> (Vec<u8>, Vec<u8>) {
+pub fn client_handshake_input(conn_id: u64) -> AeadInput {
     let shared_key = SigningKey::new(&SHA256, &client_handshake_secret(conn_id));
-    let key = qhkdf_expand(&shared_key, b"key", AES_128_GCM.key_len());
-    let iv = qhkdf_expand(&shared_key, b"iv", AES_128_GCM.nonce_len());
-    (key, iv)
+    let mut input = AeadInput::new(&AES_128_GCM);
+    qhkdf_expand(&shared_key, b"key", input.key_mut());
+    qhkdf_expand(&shared_key, b"iv", input.nonce_mut());
+    input
 }
 
 fn client_handshake_secret(conn_id: u64) -> Vec<u8> {
     let prk = handshake_secret(conn_id);
-    qhkdf_expand(&prk, b"client hs", SHA256.output_len)
+    let mut out = vec![0u8; SHA256.output_len];
+    qhkdf_expand(&prk, b"client hs", &mut out);
+    out
 }
 
-pub fn qhkdf_expand(key: &SigningKey, label: &[u8], len: usize) -> Vec<u8> {
-    let mut info = Vec::with_capacity(2 + 1 + 5 + label.len());
-    info.put_u16::<BigEndian>(len as u16);
+pub fn qhkdf_expand(key: &SigningKey, label: &[u8], out: &mut [u8]) {
+    let mut info = Vec::with_capacity(2 + 1 + 5 + out.len());
+    info.put_u16::<BigEndian>(out.len() as u16);
     info.put_u8(5 + (label.len() as u8));
     info.extend_from_slice(b"QUIC ");
     info.extend_from_slice(&label);
-
-    let mut out = vec![0u8; len];
-    hkdf::expand(key, &info, &mut out);
-    out
+    hkdf::expand(key, &info, out);
 }
 
 fn handshake_secret(conn_id: u64) -> SigningKey {
@@ -41,6 +36,42 @@ fn handshake_secret(conn_id: u64) -> SigningKey {
     let mut buf = Vec::with_capacity(8);
     buf.put_u64::<BigEndian>(conn_id);
     hkdf::extract(&key, &buf)
+}
+
+pub struct AeadInput {
+    alg: &'static aead::Algorithm,
+    input: Vec<u8>,
+    split: usize,
+}
+
+impl AeadInput {
+    pub fn new(alg: &'static aead::Algorithm) -> AeadInput {
+        AeadInput {
+            alg,
+            input: vec![0; alg.key_len() + alg.nonce_len()],
+            split: alg.key_len(),
+        }
+    }
+
+    pub fn algorithm(&self) -> &aead::Algorithm {
+        self.alg
+    }
+
+    pub fn key(&self) -> &[u8] {
+        &self.input[..self.split]
+    }
+
+    pub fn key_mut(&mut self) -> &mut [u8] {
+        &mut self.input[..self.split]
+    }
+
+    pub fn nonce(&self) -> &[u8] {
+        &self.input[self.split..]
+    }
+
+    pub fn nonce_mut(&mut self) -> &mut [u8] {
+        &mut self.input[self.split..]
+    }
 }
 
 const HANDSHAKE_SALT: &[u8; 20] =
@@ -55,8 +86,8 @@ mod tests {
         let expected = b"\x83\x55\xf2\x1a\x3d\x8f\x83\xec\xb3\xd0\xf9\x71\x08\xd3\xf9\x5e\
                          \x0f\x65\xb4\xd8\xae\x88\xa0\x61\x1e\xe4\x9d\xb0\xb5\x23\x59\x1d";
         assert_eq!(&client_handshake_secret, expected);
-        let (key, iv) = super::client_handshake_key_input(conn_id);
-        assert_eq!(&key, b"\x3a\xd0\x54\x2c\x4a\x85\x84\x74\x00\x63\x04\x9e\x3b\x3c\xaa\xb2");
-        assert_eq!(&iv, b"\xd1\xfd\x26\x05\x42\x75\x3a\xba\x38\x58\x9b\xad");
+        let input = super::client_handshake_input(conn_id);
+        assert_eq!(input.key(), b"\x3a\xd0\x54\x2c\x4a\x85\x84\x74\x00\x63\x04\x9e\x3b\x3c\xaa\xb2");
+        assert_eq!(input.nonce(), b"\xd1\xfd\x26\x05\x42\x75\x3a\xba\x38\x58\x9b\xad");
     }
 }
