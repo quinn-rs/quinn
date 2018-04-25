71e5ab38d7ee60d840483264ef09f64f34328844
Dirkjan Ochtman <dirkjan@ochtman.nl>
2018-04-25 15:53:53 +0200

    Don't return from poll() while making progress

--- src/client.rs
+++ src/client.rs
@@ -68,62 +68,72 @@ impl Future for ConnectFuture {
     type Item = ();
     type Error = io::Error;
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        let mut waiting;
         let mut new = None;
-        if let ConnectFutureState::InitialSent(ref mut future) = self.state {
-            let (sock, mut buf) = try_ready!(future.poll());
-            let size = buf.capacity();
-            buf.resize(size, 0);
-            new = Some(ConnectFutureState::WaitingForResponse(sock.recv_dgram(buf)));
-        };
-        if let Some(state) = new.take() {
-            self.state = state;
-        }
-
-        if let ConnectFutureState::WaitingForResponse(ref mut future) = self.state {
-            let (sock, mut buf, len, addr) = try_ready!(future.poll());
-            buf.truncate(len);
-
-            let key_type = KeyType::ServerHandshake(self.endpoint.hs_cid);
-            let packet = Packet::decode(key_type, &mut buf);
-            self.endpoint.dst_cid = packet.conn_id().unwrap();
-            let tls_frame = packet.payload.iter().filter_map(|f| {
-                match *f {
-                    Frame::Stream(ref f) => Some(f),
-                    _ => None,
-                }
-            }).next().unwrap();
-            let tls = self.tls.process_handshake_messages(&tls_frame.data).unwrap();
-
-            let rsp = Packet {
-                header: Header::Long {
-                    ptype: LongType::Handshake,
-                    conn_id: packet.conn_id().unwrap(),
-                    version: DRAFT_10,
-                    number: self.endpoint.src_pn,
-                },
-                payload: vec![
-                    Frame::Ack(AckFrame {
-                        largest: packet.number(),
-                        ack_delay: 0,
-                        blocks: vec![Ack::Ack(0)],
-                    }),
-                    Frame::Stream(StreamFrame {
-                        id: 0,
-                        fin: false,
-                        offset: 0,
-                        len: Some(tls.len() as u64),
-                        data: tls,
-                    }),
-                ],
+        loop {
+            waiting = true;
+            if let ConnectFutureState::InitialSent(ref mut future) = self.state {
+                let (sock, mut buf) = try_ready!(future.poll());
+                let size = buf.capacity();
+                buf.resize(size, 0);
+                new = Some(ConnectFutureState::WaitingForResponse(sock.recv_dgram(buf)));
             };
+            if let Some(state) = new.take() {
+                waiting = false;
+                self.state = state;
+            }
 
-            self.endpoint.src_pn += 1;
-            let key = PacketKey::for_client_handshake(self.endpoint.hs_cid);
-            rsp.encode(&key, &mut buf);
-            new = Some(ConnectFutureState::InitialSent(sock.send_dgram(buf, &addr)));
-        };
-        if let Some(state) = new.take() {
-            self.state = state;
+            if let ConnectFutureState::WaitingForResponse(ref mut future) = self.state {
+                let (sock, mut buf, len, addr) = try_ready!(future.poll());
+                buf.truncate(len);
+
+                let key_type = KeyType::ServerHandshake(self.endpoint.hs_cid);
+                let packet = Packet::decode(key_type, &mut buf);
+                self.endpoint.dst_cid = packet.conn_id().unwrap();
+                let tls_frame = packet.payload.iter().filter_map(|f| {
+                    match *f {
+                        Frame::Stream(ref f) => Some(f),
+                        _ => None,
+                    }
+                }).next().unwrap();
+                let tls = self.tls.process_handshake_messages(&tls_frame.data).unwrap();
+
+                let rsp = Packet {
+                    header: Header::Long {
+                        ptype: LongType::Handshake,
+                        conn_id: packet.conn_id().unwrap(),
+                        version: DRAFT_10,
+                        number: self.endpoint.src_pn,
+                    },
+                    payload: vec![
+                        Frame::Ack(AckFrame {
+                            largest: packet.number(),
+                            ack_delay: 0,
+                            blocks: vec![Ack::Ack(0)],
+                        }),
+                        Frame::Stream(StreamFrame {
+                            id: 0,
+                            fin: false,
+                            offset: 0,
+                            len: Some(tls.len() as u64),
+                            data: tls,
+                        }),
+                    ],
+                };
+
+                self.endpoint.src_pn += 1;
+                let key = PacketKey::for_client_handshake(self.endpoint.hs_cid);
+                rsp.encode(&key, &mut buf);
+                new = Some(ConnectFutureState::InitialSent(sock.send_dgram(buf, &addr)));
+            };
+            if let Some(state) = new.take() {
+                waiting = false;
+                self.state = state;
+            }
+
+            if waiting {
+                break;
+            }
         }
 
         Ok(Async::NotReady)
