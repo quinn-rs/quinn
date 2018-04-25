8634105ac92308cb937af7f5e79de59afedfa241
Dirkjan Ochtman <dirkjan@ochtman.nl>
2018-04-25 13:42:14 +0200

    Rebuild client connect as a future

--- examples/client.rs
+++ examples/client.rs
@@ -1,9 +1,11 @@
+extern crate futures;
 extern crate quinn;
+
+use futures::Future;
 
 use std::env;
 
 fn main() {
-    let server = env::args().nth(1).unwrap();
-    let mut client = quinn::Client::new();
-    client.connect(&server, 4433);
+    let server = env::args().nth(1).expect("need server name as an argument");
+    println!("RESULT: {:?}", quinn::QuicStream::connect(&server, 4433).wait());
 }
