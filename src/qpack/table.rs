826ed328d7e01a1a26fc90cc5a8ff6f26bd6f20c
evolix1 <evolix1@gmail.com>
2018-05-27 16:38:02 +0100

    Use Cow to prepare for static table

--- src/qpack/table.rs
+++ src/qpack/table.rs
@@ -2,22 +2,30 @@
 // TODO remove allow dead code
 #![allow(dead_code)]
 
+use std::borrow::Cow;
 use std::collections::VecDeque;
 
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct HeaderField {
-    // TODO use Cow to avoid copy when possible
-    pub name: String,
-    pub value: String,
+    pub name: Cow<'static, str>,
+    pub value: Cow<'static, str>
 }
 
 
 impl HeaderField {
-    pub fn new<T>(name: T, value: T) -> HeaderField where T: Into<String> {
+    pub fn new<T>(name: T, value: T) -> HeaderField
+        where T: Into<String> {
         HeaderField {
-            name: name.into(),
-            value: value.into()
+            name: Cow::Owned(name.into()),
+            value: Cow::Owned(value.into())
+        }
+    }
+
+    pub fn from_static(name: &'static str, value: &'static str) -> HeaderField {
+        HeaderField {
+            name: Cow::Borrowed(name),
+            value: Cow::Borrowed(value)
         }
     }
 }
