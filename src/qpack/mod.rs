9e6ee3e64655f4395ce0fb95703c09cd9a6c5e43
evolix1 <evolix1@gmail.com>
2018-05-27 15:51:29 +0100

    Moved qpack into module

---
+++ src/qpack/mod.rs
@@ -0,0 +1,9 @@
+// This is only here because qpack is new and quinn no uses it yet.
+// TODO remove allow dead code
+#![allow(unused_imports)]
+
+pub mod table;
+use self::table::{HeaderField, HeaderTable};
+
+pub mod dyn_table;
+use self::dyn_table::DynamicTable;
