ba5f5eb4b1d0d56439392027454cd3664fe0868e
evolix1 <evolix1@gmail.com>
2018-06-10 13:38:34 +0100

    Moved string decoding to module

---
+++ quinn-h3/src/qpack/string/bitrange.rs
@@ -0,0 +1,32 @@
+#[derive(Debug, PartialEq, Clone)]
+pub struct BitRange {
+    pub byte: u32,
+    pub bit: u32,
+    pub count: u32
+}
+
+
+impl BitRange {
+
+    pub fn new() -> BitRange {
+        BitRange { byte: 0, bit: 0, count: 0 }
+    }
+
+    pub fn forwards(&mut self, step: u32) {
+        self.bit += self.count;
+        
+        self.byte += self.bit / 8;;
+        self.bit %= 8;
+        
+        self.count = step;
+    }
+
+    pub fn byte_boundary(&self) -> BitRange {
+        BitRange {
+            byte: self.byte,
+            bit: self.bit,
+            count: 8 - (self.bit % 8)
+        }
+    }
+
+}
