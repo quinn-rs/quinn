02a0a4225a30e2e465ca75e59a3aa930aff64102
evolix1 <evolix1@gmail.com>
2018-08-09 15:48:39 +0100

    Refurbish test on tables to better express my intention
    * Also fix implementation of table size

--- quinn-h3/src/qpack/table/field.rs
+++ quinn-h3/src/qpack/table/field.rs
@@ -5,6 +5,13 @@
 use std::borrow::Cow;
 
 
+/**
+ * https://tools.ietf.org/html/rfc7541
+ * 4.1.  Calculating Table Size
+ */
+pub const ESTIMATED_OVERHEAD_BYTES: usize = 32;
+
+
 #[derive(Debug, PartialEq, Clone)]
 pub struct HeaderField {
     pub name: Cow<'static, [u8]>,
@@ -23,6 +30,29 @@ impl HeaderField {
     }
     
     pub fn mem_size(&self) -> usize {
-        self.name.len() + self.value.len()
+        self.name.len() + self.value.len() + ESTIMATED_OVERHEAD_BYTES
     }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    /**
+     * https://tools.ietf.org/html/rfc7541#section-4.1
+     * "The size of an entry is the sum of its name's length in octets (as
+     *  defined in Section 5.2), its value's length in octets, and 32."
+     * "The size of an entry is calculated using the length of its name and
+     *  value without any Huffman encoding applied."
+     */
+    #[test]
+    fn test_field_size_is_offset_by_32() {
+        let field = HeaderField { 
+            name: Cow::Borrowed(b"Name"),
+            value: Cow::Borrowed(b"Value")
+        };
+        assert_eq!(field.mem_size(), 4 + 5 + 32);
+    }
+
 }
