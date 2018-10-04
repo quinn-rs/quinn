ac7424b15122c2818f81d3b632a9c772a0c0553d
Simon Heath <icefoxen@gmail.com>
2018-10-04 19:38:31 -0400

    Remove lingering references to quicr.

--- shell.nix
+++
@@ -1,9 +0,0 @@
-with import <nixpkgs> { };
-in stdenv.mkDerivation {
-  name = "quicr";
-  buildInputs = with pkgs; [ rustChannels.stable.rust ];
-  shellHook = ''
-    export CARGO_INCREMENTAL=1
-    export RUST_BACKTRACE=1
-  '';
-}
