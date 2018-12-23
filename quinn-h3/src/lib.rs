ceed37e2edd18a98c17ad917a4c92d7b04b34590
stammw <begue.jc@gmail.com>
2018-12-23 14:07:11 +0100

    QPACK: retreive fields by name in static table

--- quinn-h3/src/lib.rs
+++ quinn-h3/src/lib.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate lazy_static;
+
 #[cfg(test)]
 #[macro_use]
 extern crate proptest;
--- quinn-h3/src/qpack/table/static_.rs
+++ quinn-h3/src/qpack/table/static_.rs
@@ -3,14 +3,15 @@
 #![allow(dead_code)]
 
 use std::borrow::Cow;
+use std::collections::HashMap;
 
 use super::field::HeaderField;
-
 
 #[allow(unused_macros)]
 macro_rules! decl_fields {
     [ $( ($key:expr, $value:expr) ),* ] => {
-        [ $(
+        [
+            $(
             HeaderField {
                 name: Cow::Borrowed($key),
                 value: Cow::Borrowed($value)
@@ -19,109 +20,188 @@ macro_rules! decl_fields {
     }
 }
 
-const PREDEFINED_HEADERS: [HeaderField; 61] = decl_fields![
+const PREDEFINED_HEADERS: [HeaderField; 99] = decl_fields![
     (b":authority", b""),
-    (b":method", b"GET"),
-    (b":method", b"POST"),
     (b":path", b"/"),
-    (b":path", b"/index.html"),
-    (b":scheme", b"http"),
-    (b":scheme", b"https"),
-    (b":status", b"200"),
-    (b":status", b"204"),
-    (b":status", b"206"),
-    (b":status", b"304"),
-    (b":status", b"400"),
-    (b":status", b"404"),
-    (b":status", b"500"),
-    (b"accept-charset", b""),
-    (b"accept-encoding", b"gzip, deflate"),
-    (b"accept-language", b""),
-    (b"accept-ranges", b""),
-    (b"accept", b""),
-    (b"access-control-allow-origin", b""),
-    (b"age", b""),
-    (b"allow", b""),
-    (b"authorization", b""),
-    (b"cache-control", b""),
+    (b"age", b"0"),
     (b"content-disposition", b""),
-    (b"content-encoding", b""),
-    (b"content-language", b""),
-    (b"content-length", b""),
-    (b"content-location", b""),
-    (b"content-range", b""),
-    (b"content-type", b""),
+    (b"content-length", b"0"),
     (b"cookie", b""),
     (b"date", b""),
     (b"etag", b""),
-    (b"expect", b""),
-    (b"expires", b""),
-    (b"from", b""),
-    (b"host", b""),
-    (b"if-match", b""),
     (b"if-modified-since", b""),
     (b"if-none-match", b""),
-    (b"if-range", b""),
-    (b"if-unmodified-since", b""),
     (b"last-modified", b""),
     (b"link", b""),
     (b"location", b""),
-    (b"max-forwards", b""),
-    (b"proxy-authenticate", b""),
-    (b"proxy-authorization", b""),
-    (b"range", b""),
     (b"referer", b""),
-    (b"refresh", b""),
-    (b"retry-after", b""),
-    (b"server", b""),
     (b"set-cookie", b""),
-    (b"strict-transport-security", b""),
-    (b"transfer-encoding", b""),
+    (b":method", b"CONNECT"),
+    (b":method", b"DELETE"),
+    (b":method", b"GET"),
+    (b":method", b"HEAD"),
+    (b":method", b"OPTIONS"),
+    (b":method", b"POST"),
+    (b":method", b"PUT"),
+    (b":scheme", b"http"),
+    (b":scheme", b"https"),
+    (b":status", b"103"),
+    (b":status", b"200"),
+    (b":status", b"304"),
+    (b":status", b"404"),
+    (b":status", b"503"),
+    (b"accept", b"*/*"),
+    (b"accept", b"application/dns-message"),
+    (b"accept-encoding", b"gzip, deflate, br"),
+    (b"accept-ranges", b"bytes"),
+    (b"access-control-allow-headers", b"cache-control"),
+    (b"access-control-allow-headers", b"content-type"),
+    (b"access-control-allow-origin", b"*"),
+    (b"cache-control", b"max-age=0"),
+    (b"cache-control", b"max-age=2592000"),
+    (b"cache-control", b"max-age=604800"),
+    (b"cache-control", b"no-cache"),
+    (b"cache-control", b"no-store"),
+    (b"cache-control", b"public, max-age=31536000"),
+    (b"content-encoding", b"br"),
+    (b"content-encoding", b"gzip"),
+    (b"content-type", b"application/dns-message"),
+    (b"content-type", b"application/javascript"),
+    (b"content-type", b"application/json"),
+    (b"content-type", b"application/x-www-form-urlencoded"),
+    (b"content-type", b"image/gif"),
+    (b"content-type", b"image/jpeg"),
+    (b"content-type", b"image/png"),
+    (b"content-type", b"text/css"),
+    (b"content-type", b"text/html; charset=utf-8"),
+    (b"content-type", b"text/plain"),
+    (b"content-type", b"text/plain;charset=utf-8"),
+    (b"range", b"bytes=0-"),
+    (b"strict-transport-security", b"max-age=31536000"),
+    (
+        b"strict-transport-security",
+        b"max-age=31536000; includesubdomains"
+    ),
+    (
+        b"strict-transport-security",
+        b"max-age=31536000; includesubdomains; preload"
+    ),
+    (b"vary", b"accept-encoding"),
+    (b"vary", b"origin"),
+    (b"x-content-type-options", b"nosniff"),
+    (b"x-xss-protection", b"1; mode=block"),
+    (b":status", b"100"),
+    (b":status", b"204"),
+    (b":status", b"206"),
+    (b":status", b"302"),
+    (b":status", b"400"),
+    (b":status", b"403"),
+    (b":status", b"421"),
+    (b":status", b"425"),
+    (b":status", b"500"),
+    (b"accept-language", b""),
+    (b"access-control-allow-credentials", b"FALSE"),
+    (b"access-control-allow-credentials", b"TRUE"),
+    (b"access-control-allow-headers", b"*"),
+    (b"access-control-allow-methods", b"get"),
+    (b"access-control-allow-methods", b"get, post, options"),
+    (b"access-control-allow-methods", b"options"),
+    (b"access-control-expose-headers", b"content-length"),
+    (b"access-control-request-headers", b"content-type"),
+    (b"access-control-request-method", b"get"),
+    (b"access-control-request-method", b"post"),
+    (b"alt-svc", b"clear"),
+    (b"authorization", b""),
+    (
+        b"content-security-policy",
+        b"script-src 'none'; object-src 'none'; base-uri 'none'"
+    ),
+    (b"early-data", b"1"),
+    (b"expect-ct", b""),
+    (b"forwarded", b""),
+    (b"if-range", b""),
+    (b"origin", b""),
+    (b"purpose", b"prefetch"),
+    (b"server", b""),
+    (b"timing-allow-origin", b"*"),
+    (b"upgrade-insecure-requests", b"1"),
     (b"user-agent", b""),
-    (b"vary", b""),
-    (b"via", b""),
-    (b"www-authenticate", b"")
+    (b"x-forwarded-for", b""),
+    (b"x-frame-options", b"deny"),
+    (b"x-frame-options", b"sameorigin")
 ];
 
+lazy_static! {
+    static ref PREDEFINED_HEADERS_MAP: HashMap<Cow<'static, [u8]>, (usize, &'static HeaderField)> =
+        PREDEFINED_HEADERS
+            .iter()
+            .enumerate()
+            .map(|(idx, field)| (field.name.clone(), (idx, field)))
+            .collect();
+}
 
 pub struct StaticTable {}
 
-
 impl StaticTable {
     pub fn get(index: usize) -> Option<&'static HeaderField> {
-        if index == 0 { None }
-        else { PREDEFINED_HEADERS.get(index - 1) }
+        if index > Self::count() - 1 {
+            return None;
+        }
+        PREDEFINED_HEADERS.get(index)
     }
-    
+
     pub fn count() -> usize {
         PREDEFINED_HEADERS.len()
     }
-}
 
+    pub fn find(name: &[u8]) -> Option<&'static (usize, &'static HeaderField)> {
+        PREDEFINED_HEADERS_MAP.get(name)
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     /**
-     * https://tools.ietf.org/html/draft-ietf-quic-qpack-01
-     * 2.1. Static Table
-     *    [...] Note that because HPACK did not use zero-based references, 
-     *    there is no value at index zero of the static table.
+     * https://tools.ietf.org/html/draft-ietf-quic-qpack-05
+     *  3.1.  Static Table
+     *  [...]
+     *  Note the QPACK static table is indexed from 0, whereas the HPACK
+     *  static table is indexed from 1.
      */
     #[test]
-    fn test_static_table_index_is_1_based() {
-        assert_eq!(StaticTable::get(0), None);
+    fn test_static_table_index_is_0_based() {
+        assert_eq!(
+            StaticTable::get(0),
+            Some(&HeaderField::new(":authority", ""))
+        );
     }
 
     #[test]
     fn test_static_table_is_full() {
-        assert_eq!(StaticTable::count(), 61);
+        assert_eq!(StaticTable::count(), 99);
     }
 
     #[test]
     fn test_static_table_can_get_field() {
-        let field = HeaderField::new("www-authenticate", "");
-        assert_eq!(StaticTable::get(61), Some(&field));
+        assert_eq!(
+            StaticTable::get(98),
+            Some(&HeaderField::new("x-frame-options", "sameorigin"))
+        );
+    }
+
+    #[test]
+    fn invalid_index() {
+        assert_eq!(StaticTable::get(99), None);
+    }
+
+    #[test]
+    fn find_by_name() {
+        assert_eq!(
+            StaticTable::find(b"last-modified"),
+            Some(&(10usize, &HeaderField::new("last-modified", "")))
+        );
+        assert_eq!(StaticTable::find(b"does-not-exist"), None);
     }
 }
