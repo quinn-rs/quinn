bea8e65f01d33b97e56da5c900d3b14bdc714746
Benjamin Saunders <ben.e.saunders@gmail.com>
2019-01-04 00:32:03 -0800

    Fix feature-gated type reference

--- quinn/src/lib.rs
+++ quinn/src/lib.rs
@@ -469,7 +469,7 @@ impl ClientConfigBuilder {
             fn verify_server_cert(
                 &self,
                 _roots: &rustls::RootCertStore,
-                _presented_certs: &[Certificate],
+                _presented_certs: &[rustls::Certificate],
                 _dns_name: webpki::DNSNameRef,
                 _ocsp_response: &[u8],
             ) -> Result<rustls::ServerCertVerified, TLSError> {
