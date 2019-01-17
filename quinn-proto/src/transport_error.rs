ae328247d0623d26cdc47ff7132ba89a84c6716a
Benjamin Saunders <ben.e.saunders@gmail.com>
2019-01-16 21:09:10 -0800

    More concise transport error description phrasing

--- quinn-proto/src/transport_error.rs
+++ quinn-proto/src/transport_error.rs
@@ -74,13 +74,13 @@ errors! {
     NO_ERROR(0x0) "the connection is being closed abruptly in the absence of any error";
     INTERNAL_ERROR(0x1) "the endpoint encountered an internal error and cannot continue with the connection";
     SERVER_BUSY(0x2) "the server is currently busy and does not accept any new connections";
-    FLOW_CONTROL_ERROR(0x3) "an endpoint received more data than it permitted in its advertised data limits";
-    STREAM_LIMIT_ERROR(0x4) "an endpoint received a frame for a stream identifier that exceeded its advertised stream limit for the corresponding stream type";
-    STREAM_STATE_ERROR(0x5) "an endpoint received a frame for a stream that was not in a state that permitted that frame";
-    FINAL_OFFSET_ERROR(0x6) "an endpoint received a STREAM frame containing data that exceeded the previously established final offset, or a RST_STREAM frame containing a final offset that was lower than the maximum offset of data that was already received, or a RST_STREAM frame containing a different final offset to the one already established";
-    FRAME_ENCODING_ERROR(0x7) "an endpoint received a frame that was badly formatted";
-    TRANSPORT_PARAMETER_ERROR(0x8) "an endpoint received transport parameters that were badly formatted, included an invalid value, was absent even though it is mandatory, was present though it is forbidden, or is otherwise in error";
-    VERSION_NEGOTIATION_ERROR(0x9) "an endpoint received transport parameters that contained version negotiation parameters that disagreed with the version negotiation that it performed, constituting a potential version downgrade attack";
-    PROTOCOL_VIOLATION(0xA) "an endpoint detected an error with protocol compliance that was not covered by more specific error codes";
-    INVALID_MIGRATION(0xC) "an endpoint received a PATH_RESPONSE frame that did not correspond to any PATH_CHALLENGE frame that it previously sent";
+    FLOW_CONTROL_ERROR(0x3) "received more data than permitted in advertised data limits";
+    STREAM_LIMIT_ERROR(0x4) "received a frame for a stream identifier that exceeded advertised the stream limit for the corresponding stream type";
+    STREAM_STATE_ERROR(0x5) "received a frame for a stream that was not in a state that permitted that frame";
+    FINAL_OFFSET_ERROR(0x6) "received a STREAM frame containing data that exceeded the previously established final offset, or a RST_STREAM frame containing a final offset that was lower than the maximum offset of data that was already received, or a RST_STREAM frame containing a different final offset to the one already established";
+    FRAME_ENCODING_ERROR(0x7) "received a frame that was badly formatted";
+    TRANSPORT_PARAMETER_ERROR(0x8) "received transport parameters that were badly formatted, included an invalid value, was absent even though it is mandatory, was present though it is forbidden, or is otherwise in error";
+    VERSION_NEGOTIATION_ERROR(0x9) "received transport parameters that contained version negotiation parameters that disagreed with the version negotiation that was performed, constituting a potential version downgrade attack";
+    PROTOCOL_VIOLATION(0xA) "detected an error with protocol compliance that was not covered by more specific error codes";
+    INVALID_MIGRATION(0xC) "received a PATH_RESPONSE frame that did not correspond to any PATH_CHALLENGE frame that it previously sent";
 }
