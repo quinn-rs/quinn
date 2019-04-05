c1106d07f8bdda2d2bafcb6de6b5a87e008a7cd2
Benjamin Saunders <ben.e.saunders@gmail.com>
2019-04-04 22:58:29 -0700

    Connection::close by reference

--- quinn/src/connection.rs
+++ quinn/src/connection.rs
@@ -176,7 +176,7 @@ impl Connection {
     ///
     /// `reason` will be truncated to fit in a single packet with overhead; to improve odds that it
     /// is preserved in full, it should be kept under 1KiB.
-    pub fn close(self, error_code: u16, reason: &[u8]) {
+    pub fn close(&self, error_code: u16, reason: &[u8]) {
         let conn = &mut *self.0.lock().unwrap();
         conn.close(error_code, reason);
     }
