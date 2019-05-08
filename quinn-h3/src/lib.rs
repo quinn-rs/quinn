18cef2753614b3f3aab759b0b7d99a1740a13596
stammw <begue.jc@gmail.com>
2019-05-08 13:37:27 +0200

    H3: builders for client and server

--- quinn-h3/src/lib.rs
+++ quinn-h3/src/lib.rs
@@ -9,6 +9,16 @@ extern crate proptest;
 #[macro_use]
 extern crate assert_matches;
 
+pub mod client;
 mod frame;
 pub mod proto;
 pub mod qpack;
+pub mod server;
+
+#[derive(Clone, Default)]
+pub struct Settings {
+    pub max_header_list_size: u64,
+    pub num_placeholders: u64,
+    pub qpack_max_table_capacity: u64,
+    pub qpack_blocked_streams: u64,
+}
