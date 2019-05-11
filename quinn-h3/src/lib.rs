bd8cc901e6fb5c7551ee960582a45930f0af1983
stammw <begue.jc@gmail.com>
2019-05-11 15:04:27 +0200

    H3: make max_header_list_size unlimited by default

--- quinn-h3/src/lib.rs
+++ quinn-h3/src/lib.rs
@@ -16,10 +16,21 @@ pub mod proto;
 pub mod qpack;
 pub mod server;
 
-#[derive(Clone, Default)]
+#[derive(Clone)]
 pub struct Settings {
     pub max_header_list_size: u64,
     pub num_placeholders: u64,
     pub qpack_max_table_capacity: u64,
     pub qpack_blocked_streams: u64,
+}
+
+impl Default for Settings {
+    fn default() -> Self {
+        Self {
+            max_header_list_size: u64::max_value(),
+            num_placeholders: 0,
+            qpack_max_table_capacity: 0,
+            qpack_blocked_streams: 0,
+        }
+    }
 }
