92a3f20842c0ca676de018c8965c362d68640eff
stammw <begue.jc@gmail.com>
2019-05-12 16:33:15 +0200

    H3: incoming request stream

--- quinn-h3/src/connection.rs
+++ quinn-h3/src/connection.rs
@@ -1,12 +1,15 @@
-#![allow(dead_code)]
-
+use std::collections::VecDeque;
 use std::sync::{Arc, Mutex};
+
+use futures::task::Task;
+use futures::{Async, Future, Poll, Stream};
+use quinn::{NewStream, RecvStream, SendStream};
+use slog::{info, Logger};
 
 use crate::{
     proto::connection::{Connection, Error as ProtoError},
-    Settings,
+    Error, Settings,
 };
-use slog::Logger;
 
 pub struct ConnectionDriver {
     conn: ConnectionRef,
@@ -24,16 +27,50 @@ impl ConnectionDriver {
     }
 }
 
+impl Future for ConnectionDriver {
+    type Item = ();
+    type Error = Error;
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        match self.incoming.poll()? {
+            Async::Ready(None) => return Err(Error::peer("incoming requests closed")),
+            Async::Ready(Some(NewStream::Uni(_recv))) => {
+                info!(self.log, "incoming uni stream ignored");
+            }
+            Async::Ready(Some(NewStream::Bi(send, recv))) => {
+                let mut conn = self.conn.h3.lock().unwrap();
+                conn.requests.push_back((send, recv));
+                if let Some(ref t) = conn.requests_task {
+                    t.notify();
+                }
+            }
+            _ => (),
+        }
+
+        Ok(Async::NotReady)
+    }
+}
+
+pub(crate) struct ConnectionInner {
+    pub inner: Connection,
+    pub requests: VecDeque<(SendStream, RecvStream)>,
+    pub requests_task: Option<Task>,
+}
+
 #[derive(Clone)]
 pub(crate) struct ConnectionRef {
-    pub inner: Arc<Mutex<Connection>>,
+    pub h3: Arc<Mutex<ConnectionInner>>,
     pub quic: quinn::Connection,
 }
 
 impl ConnectionRef {
     pub fn new(quic: quinn::Connection, settings: Settings) -> Result<Self, ProtoError> {
         Ok(Self {
-            inner: Arc::new(Mutex::new(Connection::with_settings(settings)?)),
+            h3: Arc::new(Mutex::new(ConnectionInner {
+                inner: Connection::with_settings(settings)?,
+                requests: VecDeque::with_capacity(16),
+                requests_task: None,
+            })),
             quic,
         })
     }
