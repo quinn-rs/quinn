4e72147fc5466ad7a2e18a8c7044cee6c0319196
Benjamin Saunders <ben.e.saunders@gmail.com>
2019-11-17 18:09:42 -0800

    Per-space PTO

--- quinn-proto/src/spaces.rs
+++ quinn-proto/src/spaces.rs
@@ -50,10 +50,15 @@ where
     /// Current offset of outgoing cryptographic handshake stream
     pub(crate) crypto_offset: u64,
 
+    /// The time the most recently sent retransmittable packet was sent.
+    pub(crate) time_of_last_sent_ack_eliciting_packet: Option<Instant>,
     /// The time at which the earliest sent packet in this space will be considered lost based on
     /// exceeding the reordering window in time. Only set for packets numbered prior to a packet
     /// that has been acknowledged.
     pub(crate) loss_time: Option<Instant>,
+    /// Number of tail loss probes to send
+    pub(crate) loss_probes: u32,
+    pub(crate) ping_pending: bool,
 }
 
 impl<K> PacketSpace<K>
@@ -80,7 +85,10 @@ where
             crypto_stream: Assembler::new(),
             crypto_offset: 0,
 
+            time_of_last_sent_ack_eliciting_packet: None,
             loss_time: None,
+            loss_probes: 0,
+            ping_pending: false,
         }
     }
 
@@ -93,7 +101,9 @@ where
     }
 
     pub(crate) fn can_send(&self) -> bool {
-        !self.pending.is_empty() || (self.permit_ack_only && !self.pending_acks.is_empty())
+        !self.pending.is_empty()
+            || (self.permit_ack_only && !self.pending_acks.is_empty())
+            || self.ping_pending
     }
 
     /// Verifies sanity of an ECN block and returns whether congestion was encountered.
