quinn-rs/quinn

commit 9676f3e98269afa51f0a71c28836521f18c292a0

Expose a single timer per Connection
Zero-cost simplification.

quinn/src/connection.rs
@@ -15,9 +15,9 @@ use futures::{
     channel::{mpsc, oneshot},
     FutureExt, StreamExt,
 };
-use proto::{ConnectionError, ConnectionHandle, ConnectionId, Dir, StreamId, TimerUpdate};
+use proto::{ConnectionError, ConnectionHandle, ConnectionId, Dir, StreamId};
 use tokio::time::{delay_until, Delay, Instant as TokioInstant};
-use tracing::{info_span, trace};
+use tracing::info_span;
 
 use crate::{
     broadcast::{self, Broadcast},
@@ -214,8 +214,7 @@ impl Future for ConnectionDriver {
                 return Poll::Ready(Err(e));
             }
             conn.drive_transmit(now);
-            keep_going |= conn.drive_timers(cx, now);
-            keep_going |= conn.handle_timer_updates();
+            keep_going |= conn.drive_timer(cx, now);
             conn.forward_endpoint_events();
             conn.forward_app_events();
             if !keep_going || conn.inner.is_drained() {
@@ -481,13 +480,12 @@ impl ConnectionRef {
         conn_events: mpsc::UnboundedReceiver<ConnectionEvent>,
     ) -> Self {
         Self(Arc::new(Mutex::new(ConnectionInner {
-            epoch: Instant::now(),
             inner: conn,
             driver: None,
             handle,
             on_connected: None,
             connected: false,
-            timers: Default::default(),
+            timer: None,
             conn_events,
             endpoint_events,
             blocked_writers: HashMap::new(),
@@ -535,13 +533,12 @@ impl std::ops::Deref for ConnectionRef {
 }
 
 pub struct ConnectionInner {
-    epoch: Instant,
     pub(crate) inner: proto::Connection,
     driver: Option<Waker>,
     handle: ConnectionHandle,
     on_connected: Option<oneshot::Sender<bool>>,
     connected: bool,
-    timers: proto::TimerTable<Option<Delay>>,
+    timer: Option<Delay>,
     conn_events: mpsc::UnboundedReceiver<ConnectionEvent>,
     endpoint_events: mpsc::UnboundedSender<(ConnectionHandle, EndpointEvent)>,
     pub(crate) blocked_writers: HashMap<StreamId, Waker>,
@@ -660,51 +657,25 @@ impl ConnectionInner {
         }
     }
 
-    fn drive_timers(&mut self, cx: &mut Context, now: Instant) -> bool {
+    fn drive_timer(&mut self, cx: &mut Context, now: Instant) -> bool {
         let mut keep_going = false;
-        for (timer, slot) in &mut self.timers {
-            if let Some(ref mut delay) = slot {
-                match delay.poll_unpin(cx) {
-                    Poll::Ready(()) => {
-                        *slot = None;
-                        trace!("{:?} timeout", timer);
-                        self.inner.handle_timeout(now, timer);
-                        // Timeout call may have queued sends
-                        keep_going = true;
-                    }
-                    Poll::Pending => {}
-                }
-            }
+        if self.timer.is_none() {
+            self.timer = self
+                .inner
+                .poll_timeout()
+                .map(|x| delay_until(TokioInstant::from_std(x)));
         }
-        keep_going
-    }
-
-    fn handle_timer_updates(&mut self) -> bool {
-        let mut keep_going = false;
-        while let Some(update) = self.inner.poll_timers() {
-            keep_going = true; // Timers must be polled once set
-            match update {
-                TimerUpdate {
-                    timer,
-                    update: proto::TimerSetting::Start(time),
-                } => match self.timers[timer] {
-                    ref mut x @ None => {
-                        trace!(time = ?time.duration_since(self.epoch), "{:?} timer start", timer);
-                        *x = Some(delay_until(TokioInstant::from_std(time)));
-                    }
-                    Some(ref mut x) => {
-                        trace!(time = ?time.duration_since(self.epoch), "{:?} timer reset", timer);
-                        x.reset(TokioInstant::from_std(time));
-                    }
-                },
-                TimerUpdate {
-                    timer,
-                    update: proto::TimerSetting::Stop,
-                } => {
-                    if self.timers[timer].take().is_some() {
-                        trace!("{:?} timer stop", timer);
-                    }
-                }
+        while let Some(ref mut delay) = self.timer {
+            if delay.poll_unpin(cx) == Poll::Pending {
+                break;
+            }
+            self.inner.handle_timeout(now);
+            // Timeout call may have queued sends
+            keep_going = true;
+            if let Some(t) = self.inner.poll_timeout() {
+                delay.reset(TokioInstant::from_std(t));
+            } else {
+                self.timer = None;
             }
         }
         keep_going

quinn-proto/src/connection.rs
@@ -25,7 +25,7 @@ use crate::{
     },
     spaces::{CryptoSpace, PacketSpace, Retransmits, SentPacket},
     streams::{self, FinishError, ReadError, Streams, UnknownStream, WriteError},
-    timer::{Timer, TimerKind, TimerTable},
+    timer::{Timer, TimerTable},
     transport_parameters::{self, TransportParameters},
     Dir, Frame, Side, StreamId, Transmit, TransportError, TransportErrorCode, VarInt,
     MAX_STREAM_COUNT, MIN_INITIAL_SIZE, MIN_MTU, REM_CID_COUNT, RESET_TOKEN_SIZE,
@@ -115,6 +115,7 @@ where
     first_1rtt_sent: Option<u64>,
     /// Sequence number of the first remote CID that we haven't been asked to retire
     first_unretired_cid: u64,
+    timers: TimerTable,
 
     //
     // Queued non-retransmittable 1-RTT data
@@ -236,6 +237,7 @@ where
             idle_timeout: config.idle_timeout,
             first_1rtt_sent: None,
             first_unretired_cid: 0,
+            timers: TimerTable::default(),
 
             path_challenge_pending: false,
             path_response: None,
@@ -265,20 +267,15 @@ where
         this
     }
 
-    /// Returns timer updates
+    /// Returns the next time at which `handle_timeout` should be called
     ///
-    /// Connections should be polled for timer updates after:
+    /// The value returned may change after:
     /// - the application performed some I/O on the connection
     /// - an incoming packet is handled
     /// - a packet is transmitted
     /// - a timer expires
-    pub fn poll_timers(&mut self) -> Option<TimerUpdate> {
-        for (timer, update) in self.io.timers.iter_mut() {
-            if let Some(update) = update.take() {
-                return Some(TimerUpdate { timer, update });
-            }
-        }
-        None
+    pub fn poll_timeout(&mut self) -> Option<Instant> {
+        self.timers.next_timeout()
     }
 
     /// Returns application-facing events
@@ -522,35 +519,42 @@ where
     /// Executes protocol logic, potentially preparing signals (including application `Event`s,
     /// `EndpointEvent`s and outgoing datagrams) that should be extracted through the relevant
     /// methods.
-    pub fn handle_timeout(&mut self, now: Instant, timer: Timer) {
-        match timer.0 {
-            TimerKind::Close => {
-                self.state = State::Drained;
-                self.endpoint_events.push_back(EndpointEventInner::Drained);
+    pub fn handle_timeout(&mut self, now: Instant) {
+        for &timer in &Timer::VALUES {
+            if !self.timers.is_expired(timer, now) {
+                continue;
             }
-            TimerKind::Idle => {
-                self.close_common();
-                self.events.push_back(ConnectionError::TimedOut.into());
-                self.state = State::Drained;
-                self.endpoint_events.push_back(EndpointEventInner::Drained);
-            }
-            TimerKind::KeepAlive => {
-                trace!("sending keep-alive");
-                self.ping();
-            }
-            TimerKind::LossDetection => {
-                self.on_loss_detection_timeout(now);
-            }
-            TimerKind::KeyDiscard => {
-                self.zero_rtt_crypto = None;
-                self.prev_crypto = None;
-            }
-            TimerKind::PathValidation => {
-                debug!("path validation failed");
-                self.path_challenge = None;
-                self.path_challenge_pending = false;
-                if let Some(prev) = self.prev_path.take() {
-                    self.path = prev;
+            self.timers.stop(timer);
+            trace!(timer = ?timer, "timeout");
+            match timer {
+                Timer::Close => {
+                    self.state = State::Drained;
+                    self.endpoint_events.push_back(EndpointEventInner::Drained);
+                }
+                Timer::Idle => {
+                    self.close_common();
+                    self.events.push_back(ConnectionError::TimedOut.into());
+                    self.state = State::Drained;
+                    self.endpoint_events.push_back(EndpointEventInner::Drained);
+                }
+                Timer::KeepAlive => {
+                    trace!("sending keep-alive");
+                    self.ping();
+                }
+                Timer::LossDetection => {
+                    self.on_loss_detection_timeout(now);
+                }
+                Timer::KeyDiscard => {
+                    self.zero_rtt_crypto = None;
+                    self.prev_crypto = None;
+                }
+                Timer::PathValidation => {
+                    debug!("path validation failed");
+                    self.path_challenge = None;
+                    self.path_challenge_pending = false;
+                    if let Some(prev) = self.prev_path.take() {
+                        self.path = prev;
+                    }
                 }
             }
         }
@@ -568,8 +572,7 @@ where
                 .expect("update not acknowledged yet")
                 .1
         };
-        self.io
-            .timer_start(TimerKind::KeyDiscard, start + self.pto() * 3);
+        self.timers.set(Timer::KeyDiscard, start + self.pto() * 3);
     }
 
     fn on_loss_detection_timeout(&mut self, now: Instant) {
@@ -745,14 +748,14 @@ where
     fn set_loss_detection_timer(&mut self) {
         if let Some((loss_time, _)) = self.earliest_time_and_space(|x| x.loss_time) {
             // Time threshold loss detection.
-            self.io.timer_start(TimerKind::LossDetection, loss_time);
+            self.timers.set(Timer::LossDetection, loss_time);
             return;
         }
 
         // Don't arm timer if there are no ack-eliciting packets
         // in flight and the handshake is complete.
         if self.in_flight.ack_eliciting == 0 && self.peer_not_awaiting_address_validation() {
-            self.io.timer_stop(TimerKind::LossDetection);
+            self.timers.stop(Timer::LossDetection);
             return;
         }
 
@@ -761,8 +764,7 @@ where
             self.earliest_time_and_space(|x| x.time_of_last_sent_ack_eliciting_packet)
         {
             let timeout = self.pto() * 2u32.pow(cmp::min(self.pto_count, MAX_BACKOFF_EXPONENT));
-            self.io
-                .timer_start(TimerKind::LossDetection, sent_time + timeout);
+            self.timers.set(Timer::LossDetection, sent_time + timeout);
         }
     }
 
@@ -828,19 +830,19 @@ where
             return;
         }
         if self.state.is_closed() {
-            self.io.timer_stop(TimerKind::Idle);
+            self.timers.stop(Timer::Idle);
             return;
         }
         let dt = cmp::max(Duration::from_millis(self.idle_timeout), 3 * self.pto());
-        self.io.timer_start(TimerKind::Idle, now + dt);
+        self.timers.set(Timer::Idle, now + dt);
     }
 
     fn reset_keep_alive(&mut self, now: Instant) {
         if self.config.keep_alive_interval == 0 || !self.state.is_established() {
             return;
         }
-        self.io.timer_start(
-            TimerKind::KeepAlive,
+        self.timers.set(
+            Timer::KeepAlive,
             now + Duration::from_millis(u64::from(self.config.keep_alive_interval)),
         );
     }
@@ -1314,7 +1316,7 @@ where
             self.endpoint_events.push_back(EndpointEventInner::Drained);
             // Close timer may have been started previously, e.g. if we sent a close and got a
             // stateless reset in response
-            self.io.timer_stop(TimerKind::Close);
+            self.timers.stop(Timer::Close);
         }
 
         // Transmit CONNECTION_CLOSE if necessary
@@ -1721,7 +1723,7 @@ where
                         continue;
                     }
                     trace!("path validated");
-                    self.io.timer_stop(TimerKind::PathValidation);
+                    self.timers.stop(Timer::PathValidation);
                     self.path_challenge = None;
                 }
                 Frame::MaxData(bytes) => {
@@ -2032,8 +2034,8 @@ where
         }
 
         // Initiate path validation
-        self.io.timer_start(
-            TimerKind::PathValidation,
+        self.timers.set(
+            Timer::PathValidation,
             now + 3 * cmp::max(
                 self.pto(),
                 Duration::from_micros(2 * self.config.initial_rtt),
@@ -2603,13 +2605,13 @@ where
 
     fn close_common(&mut self) {
         trace!("connection closed");
-        for (_, timer) in &mut self.io.timers {
-            *timer = Some(TimerSetting::Stop);
+        for &timer in &Timer::VALUES {
+            self.timers.stop(timer);
         }
     }
 
     fn set_close_timer(&mut self, now: Instant) {
-        self.io.timer_start(TimerKind::Close, now + 3 * self.pto());
+        self.timers.set(Timer::Close, now + 3 * self.pto());
     }
 
     /// Validate transport parameters received from the peer
@@ -3056,6 +3058,17 @@ where
         self.tls.sni_hostname()
     }
 
+    /// Whether no timers but keepalive and idle are running
+    #[cfg(test)]
+    pub(crate) fn is_idle(&self) -> bool {
+        Timer::VALUES
+            .iter()
+            .filter(|&&t| t != Timer::KeepAlive)
+            .filter_map(|&t| Some((t, self.timers.get(t)?)))
+            .min_by_key(|&(_, time)| time)
+            .map_or(true, |(timer, _)| timer == Timer::Idle)
+    }
+
     /// Total number of outgoing packets that have been deemed lost
     #[cfg(test)]
     pub(crate) fn lost_packets(&self) -> u64 {
@@ -3291,45 +3304,12 @@ const MAX_ACK_BLOCKS: usize = 64;
 struct IoQueue {
     /// Whether to transmit a close packet
     close: bool,
-    /// Changes to timers
-    timers: TimerTable<Option<TimerSetting>>,
 }
 
 impl IoQueue {
     fn new() -> Self {
-        Self {
-            close: false,
-            timers: Default::default(),
-        }
+        Self { close: false }
     }
-
-    /// Start or reset a timer associated with this connection.
-    fn timer_start(&mut self, timer: TimerKind, time: Instant) {
-        self.timers[Timer(timer)] = Some(TimerSetting::Start(time));
-    }
-
-    /// Start one of the timers associated with this connection.
-    fn timer_stop(&mut self, timer: TimerKind) {
-        self.timers[Timer(timer)] = Some(TimerSetting::Stop);
-    }
-}
-
-/// Change applicable to one of a connection's timers
-#[derive(Debug, Copy, Clone)]
-pub enum TimerSetting {
-    /// Set the timer to expire at an a certain point in time
-    Start(Instant),
-    /// Cancel time timer if it's currently running
-    Stop,
-}
-
-/// Change to apply to a specific timer
-#[derive(Debug, Copy, Clone)]
-pub struct TimerUpdate {
-    /// Which timer needs an update
-    pub timer: Timer,
-    /// The new state for the timer
-    pub update: TimerSetting,
 }
 
 struct PrevCrypto<K>

quinn-proto/src/lib.rs
@@ -28,16 +28,14 @@ mod range_set;
 mod spaces;
 #[cfg(all(test, feature = "rustls"))]
 mod tests;
+mod timer;
 pub mod transport_parameters;
 mod varint;
 
 pub use varint::{VarInt, VarIntBoundsExceeded};
 
-mod timer;
-pub use timer::{Timer, TimerTable, TimerTableIter, TimerTableIterMut};
-
 mod connection;
-pub use crate::connection::{ConnectionError, Event, SendDatagramError, TimerSetting, TimerUpdate};
+pub use crate::connection::{ConnectionError, Event, SendDatagramError};
 
 pub mod crypto;
 

quinn-proto/src/tests/util.rs
@@ -16,7 +16,6 @@ use rustls::KeyLogFile;
 use tracing::{info_span, trace};
 
 use super::*;
-use crate::timer::TimerKind;
 
 pub struct Pair {
     pub server: TestEndpoint,
@@ -171,7 +170,7 @@ pub struct TestEndpoint {
     pub endpoint: Endpoint,
     pub addr: SocketAddr,
     socket: Option<UdpSocket>,
-    timers: TimerTable<Option<Instant>>,
+    timeout: Option<Instant>,
     pub outbound: VecDeque<Transmit>,
     delayed: VecDeque<Transmit>,
     pub inbound: VecDeque<(Instant, Option<EcnCodepoint>, Box<[u8]>)>,
@@ -195,7 +194,7 @@ impl TestEndpoint {
             endpoint,
             addr,
             socket,
-            timers: Default::default(),
+            timeout: None,
             outbound: VecDeque::new(),
             delayed: VecDeque::new(),
             inbound: VecDeque::new(),
@@ -242,14 +241,9 @@ impl TestEndpoint {
 
         let mut endpoint_events: Vec<(ConnectionHandle, EndpointEvent)> = vec![];
         for (ch, conn) in self.connections.iter_mut() {
-            for (timer, setting) in &mut self.timers {
-                if let Some(time) = *setting {
-                    if time <= now {
-                        trace!("{:?} timeout", timer);
-                        *setting = None;
-                        conn.handle_timeout(now, timer);
-                    }
-                }
+            if self.timeout.map_or(false, |x| x <= now) {
+                self.timeout = None;
+                conn.handle_timeout(now);
             }
 
             for (_, mut events) in self.conn_events.drain() {
@@ -265,19 +259,7 @@ impl TestEndpoint {
             while let Some(x) = conn.poll_transmit(now) {
                 self.outbound.push_back(x);
             }
-
-            while let Some(x) = conn.poll_timers() {
-                self.timers[x.timer] = match x.update {
-                    TimerSetting::Stop => {
-                        trace!("{:?} stop", x.timer);
-                        None
-                    }
-                    TimerSetting::Start(time) => {
-                        trace!("{:?} set to expire at {:?}", x.timer, time);
-                        Some(time)
-                    }
-                };
-            }
+            self.timeout = conn.poll_timeout();
         }
 
         for (ch, event) in endpoint_events {
@@ -290,14 +272,12 @@ impl TestEndpoint {
     }
 
     pub fn next_wakeup(&self) -> Option<Instant> {
-        let next_timer = self.timers.iter().filter_map(|(_, t)| *t).min();
         let next_inbound = self.inbound.front().map(|x| x.0);
-        min_opt(next_timer, next_inbound)
+        min_opt(self.timeout, next_inbound)
     }
 
     fn is_idle(&self) -> bool {
-        let t = self.next_wakeup();
-        t == self.timers[Timer(TimerKind::Idle)] || t == self.timers[Timer(TimerKind::KeepAlive)]
+        self.connections.values().all(|x| x.is_idle())
     }
 
     pub fn delay_outbound(&mut self) {

quinn-proto/src/timer.rs
@@ -1,14 +1,7 @@
-use std::{
-    ops::{Index, IndexMut},
-    slice,
-};
-
-/// Kinds of timeouts needed to run the protocol logic
-#[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
-pub struct Timer(pub(crate) TimerKind);
+use std::time::Instant;
 
 #[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
-pub(crate) enum TimerKind {
+pub(crate) enum Timer {
     /// When to send an ack-eliciting probe packet or declare unacked packets lost
     LossDetection = 0,
     /// When to close the connection after no activity
@@ -23,118 +16,42 @@ pub(crate) enum TimerKind {
     KeepAlive = 5,
 }
 
-impl TimerKind {
-    const VALUES: [Self; 6] = [
-        TimerKind::LossDetection,
-        TimerKind::Idle,
-        TimerKind::Close,
-        TimerKind::KeyDiscard,
-        TimerKind::PathValidation,
-        TimerKind::KeepAlive,
+impl Timer {
+    pub(crate) const VALUES: [Self; 6] = [
+        Timer::LossDetection,
+        Timer::Idle,
+        Timer::Close,
+        Timer::KeyDiscard,
+        Timer::PathValidation,
+        Timer::KeepAlive,
     ];
 }
 
 /// A table of data associated with each distinct kind of `Timer`
 #[derive(Debug, Copy, Clone, Default)]
-pub struct TimerTable<T> {
-    data: [T; 6],
+pub(crate) struct TimerTable {
+    data: [Option<Instant>; 6],
 }
 
-impl<T> TimerTable<T> {
-    /// Create a table initialized with the value returned by `f` for each timer
-    pub fn new(mut f: impl FnMut() -> T) -> Self {
-        Self {
-            data: [f(), f(), f(), f(), f(), f()],
-        }
+impl TimerTable {
+    pub fn set(&mut self, timer: Timer, time: Instant) {
+        self.data[timer as usize] = Some(time);
     }
 
-    /// Iterate over the contained values
-    pub fn iter(&self) -> TimerTableIter<T> {
-        TimerTableIter {
-            kind: TimerKind::VALUES.iter(),
-            table: self,
-        }
+    #[cfg(test)]
+    pub fn get(&self, timer: Timer) -> Option<Instant> {
+        self.data[timer as usize]
     }
 
-    /// Mutably iterate over the contained values
-    pub fn iter_mut(&mut self) -> TimerTableIterMut<T> {
-        TimerTableIterMut {
-            kind: TimerKind::VALUES.iter(),
-            table: self.data.iter_mut(),
-        }
+    pub fn stop(&mut self, timer: Timer) {
+        self.data[timer as usize] = None;
     }
-}
 
-/// Iterator over a `TimerTable`
-pub struct TimerTableIter<'a, T> {
-    kind: slice::Iter<'static, TimerKind>,
-    table: &'a TimerTable<T>,
-}
-
-impl<'a, T> Iterator for TimerTableIter<'a, T> {
-    type Item = (Timer, &'a T);
-    fn next(&mut self) -> Option<(Timer, &'a T)> {
-        let timer = Timer(*self.kind.next()?);
-        Some((timer, &self.table[timer]))
+    pub fn next_timeout(&self) -> Option<Instant> {
+        self.data.iter().filter_map(|&x| x).min()
     }
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        self.kind.size_hint()
-    }
-}
 
-impl<'a, T> ExactSizeIterator for TimerTableIter<'a, T> {
-    fn len(&self) -> usize {
-        self.kind.len()
-    }
-}
-
-/// Mutable iterator over a `TimerTable`
-pub struct TimerTableIterMut<'a, T> {
-    kind: slice::Iter<'static, TimerKind>,
-    table: slice::IterMut<'a, T>,
-}
-
-impl<'a, T> Iterator for TimerTableIterMut<'a, T> {
-    type Item = (Timer, &'a mut T);
-    fn next(&mut self) -> Option<(Timer, &'a mut T)> {
-        Some((Timer(*self.kind.next()?), self.table.next().unwrap()))
-    }
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        self.kind.size_hint()
-    }
-}
-
-impl<'a, T> ExactSizeIterator for TimerTableIterMut<'a, T> {
-    fn len(&self) -> usize {
-        self.kind.len()
-    }
-}
-
-impl<'a, T> IntoIterator for &'a TimerTable<T> {
-    type Item = (Timer, &'a T);
-    type IntoIter = TimerTableIter<'a, T>;
-    fn into_iter(self) -> Self::IntoIter {
-        self.iter()
-    }
-}
-
-impl<'a, T> IntoIterator for &'a mut TimerTable<T> {
-    type Item = (Timer, &'a mut T);
-    type IntoIter = TimerTableIterMut<'a, T>;
-    fn into_iter(self) -> Self::IntoIter {
-        self.iter_mut()
-    }
-}
-
-impl<T> Index<Timer> for TimerTable<T> {
-    type Output = T;
-    fn index(&self, index: Timer) -> &T {
-        &self.data[index.0 as usize]
-    }
-}
-
-impl<T> IndexMut<Timer> for TimerTable<T> {
-    fn index_mut(&mut self, index: Timer) -> &mut T {
-        &mut self.data[index.0 as usize]
+    pub fn is_expired(&self, timer: Timer, after: Instant) -> bool {
+        self.data[timer as usize].map_or(false, |x| x <= after)
     }
 }

commit d5c2a913eac7015910eb85e703c1a96f5e374054

Fix timer updates not being applied while a timer was active
Fixes a bug introduced in 9676f3e98269afa51f0a71c28836521f18c292a0.

quinn/src/connection.rs
@@ -661,23 +661,30 @@ impl ConnectionInner {
 
     fn drive_timer(&mut self, cx: &mut Context, now: Instant) -> bool {
         let mut keep_going = false;
-        if self.timer.is_none() {
-            self.timer = self
-                .inner
-                .poll_timeout()
-                .map(|x| delay_until(TokioInstant::from_std(x)));
-        }
-        while let Some(ref mut delay) = self.timer {
-            if delay.poll_unpin(cx) == Poll::Pending {
-                break;
+        loop {
+            if let Some(ref mut delay) = self.timer {
+                if delay.poll_unpin(cx) == Poll::Ready(()) {
+                    self.inner.handle_timeout(now);
+                    self.timer = None;
+                    keep_going = true;
+                }
             }
-            self.inner.handle_timeout(now);
-            // Timeout call may have queued sends
-            keep_going = true;
-            if let Some(t) = self.inner.poll_timeout() {
-                delay.reset(TokioInstant::from_std(t));
-            } else {
-                self.timer = None;
+            // Check whether we need to (re)set the timer. If so, we must poll again to ensure the
+            // timer is registered with the runtime (and check whether it's already
+            // expired). Otherwise, exit the loop.
+            match (
+                self.inner.poll_timeout().map(TokioInstant::from_std),
+                &mut self.timer,
+            ) {
+                (Some(timeout), &mut None) => self.timer = Some(delay_until(timeout)),
+                (Some(timeout), &mut Some(ref mut delay)) if delay.deadline() != timeout => {
+                    delay.reset(timeout);
+                }
+                (None, _) => {
+                    self.timer = None;
+                    break;
+                }
+                _ => break,
             }
         }
         keep_going