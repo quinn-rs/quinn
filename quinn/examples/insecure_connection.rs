42ca1cc0a55c8d769020f4c4023cb6e11c05d9d7
Benjamin Saunders <ben.e.saunders@gmail.com>
2020-01-09 20:02:15 -0800

    Simplify insecure_connection example

--- quinn/examples/insecure_connection.rs
+++ quinn/examples/insecure_connection.rs
@@ -5,83 +5,64 @@
 //! $ cargo run --example insecure_connection --features="rustls/dangerous_configuration"
 //! ```
 
-use futures::{StreamExt, TryFutureExt};
-use std::{
-    error::Error,
-    net::{Ipv4Addr, SocketAddr, SocketAddrV4, ToSocketAddrs},
-    sync::Arc,
-};
-use tokio::{
-    runtime::{Builder, Runtime},
-    task::JoinHandle,
-};
+use futures::StreamExt;
+use std::{error::Error, net::SocketAddr, sync::Arc};
 
 use quinn::{ClientConfig, ClientConfigBuilder, Endpoint};
 
 mod common;
 use common::make_server_endpoint;
 
-const SERVER_PORT: u16 = 5000;
-
-fn main() -> Result<(), Box<dyn Error>> {
-    let mut runtime = Builder::new().basic_scheduler().enable_all().build()?;
-
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     // server and client are running on the same thread asynchronously
-    run_server(&mut runtime, ("0.0.0.0", SERVER_PORT))?;
-    let handle = run_client(&mut runtime, SERVER_PORT)?;
-
-    runtime.block_on(handle)?;
+    let addr = "127.0.0.1:5000".parse().unwrap();
+    tokio::spawn(run_server(addr));
+    run_client(addr).await?;
     Ok(())
 }
 
 /// Runs a QUIC server bound to given address.
-fn run_server<A: ToSocketAddrs>(runtime: &mut Runtime, addr: A) -> Result<(), Box<dyn Error>> {
-    let (driver, mut incoming, _server_cert) = runtime.enter(|| make_server_endpoint(addr))?;
+async fn run_server(addr: SocketAddr) {
+    let (driver, mut incoming, _server_cert) = make_server_endpoint(addr).unwrap();
     // drive UDP socket
-    runtime.spawn(driver.unwrap_or_else(|e| panic!("IO error: {}", e)));
+    tokio::spawn(async { driver.await.unwrap() });
     // accept a single connection
-    runtime.spawn(async move {
-        let incoming_conn = incoming.next().await.unwrap();
-        let new_conn = incoming_conn.await.unwrap();
-        println!(
-            "[server] connection accepted: addr={}",
-            new_conn.connection.remote_address()
-        );
-        // Drive the connection to completion
-        if let Err(e) = new_conn.driver.await {
-            println!("[server] connection lost: {}", e);
-        }
-    });
-    Ok(())
+    let incoming_conn = incoming.next().await.unwrap();
+    let new_conn = incoming_conn.await.unwrap();
+    println!(
+        "[server] connection accepted: addr={}",
+        new_conn.connection.remote_address()
+    );
+    // Drive the connection to completion
+    if let Err(e) = new_conn.driver.await {
+        println!("[server] connection lost: {}", e);
+    }
 }
 
-fn run_client(runtime: &mut Runtime, server_port: u16) -> Result<JoinHandle<()>, Box<dyn Error>> {
+async fn run_client(server_addr: SocketAddr) -> Result<(), Box<dyn Error>> {
     let client_cfg = configure_client();
     let mut endpoint_builder = Endpoint::builder();
     endpoint_builder.default_client_config(client_cfg);
 
-    let (driver, endpoint, _) =
-        runtime.enter(|| endpoint_builder.bind(&"0.0.0.0:0".parse().unwrap()))?;
-    runtime.spawn(driver.unwrap_or_else(|e| panic!("IO error: {}", e)));
+    let (driver, endpoint, _) = endpoint_builder.bind(&"127.0.0.1:0".parse().unwrap())?;
+    tokio::spawn(async { driver.await.unwrap() });
 
-    let server_addr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), server_port));
     // connect to server
-    let handle = runtime.spawn(async move {
-        let quinn::NewConnection {
-            driver, connection, ..
-        } = endpoint
-            .connect(&server_addr, "localhost")
-            .unwrap()
-            .await
-            .unwrap();
-        println!("[client] connected: addr={}", connection.remote_address());
-        // Dropping handles allows the corresponding objects to automatically shut down
-        drop((endpoint, connection));
-        // Drive the connection to completion
-        driver.await.unwrap();
-    });
+    let quinn::NewConnection {
+        driver, connection, ..
+    } = endpoint
+        .connect(&server_addr, "localhost")
+        .unwrap()
+        .await
+        .unwrap();
+    println!("[client] connected: addr={}", connection.remote_address());
+    // Dropping handles allows the corresponding objects to automatically shut down
+    drop((endpoint, connection));
+    // Drive the connection to completion
+    driver.await.unwrap();
 
-    Ok(handle)
+    Ok(())
 }
 
 /// Dummy certificate verifier that treats any certificate as valid.
