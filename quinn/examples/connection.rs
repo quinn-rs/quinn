fab09dad52fa09e753092334056953f194f9368d
Benjamin Saunders <ben.e.saunders@gmail.com>
2020-02-26 20:40:00 -0800

    Fix panic in connection example

--- quinn/examples/connection.rs
+++ quinn/examples/connection.rs
@@ -31,20 +31,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             "[server] connection accepted: addr={}",
             new_conn.connection.remote_address()
         );
+        // Dropping all handles associated with a connection implicitly closes it
     });
 
     let endpoint = make_client_endpoint("0.0.0.0:0".parse().unwrap(), &[&server_cert])?;
     // connect to server
-    let quinn::NewConnection { connection, .. } = endpoint
+    let quinn::NewConnection {
+        connection,
+        mut uni_streams,
+        ..
+    } = endpoint
         .connect(&server_addr, "localhost")
         .unwrap()
         .await
         .unwrap();
     println!("[client] connected: addr={}", connection.remote_address());
 
-    drop(connection);
+    // Waiting for a stream will complete with an error when the server closes the connection
+    let _ = uni_streams.next().await;
 
-    // Make sure the server has a chance to clean up
+    // Give the server has a chance to clean up
     endpoint.wait_idle().await;
 
     Ok(())
