8894e3ee960fdc24b34f7187a108c23a1e142165
stammw <begue.jc@gmail.com>
2020-04-05 17:41:40 +0200

    H3: rework the client example for clarity

--- quinn-h3/examples/h3_client.rs
+++ quinn-h3/examples/h3_client.rs
@@ -1,15 +1,13 @@
 use std::{fs, io, net::ToSocketAddrs, path::PathBuf};
 use structopt::{self, StructOpt};
 
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use futures::AsyncReadExt;
 use http::{Request, Uri};
 use tracing::{error, info};
+use tracing_subscriber::filter::LevelFilter;
 
-use quinn_h3::{
-    self,
-    client::{Builder as ClientBuilder, Client},
-};
+use quinn_h3::client;
 
 #[derive(StructOpt, Debug, Clone)]
 #[structopt(name = "h3_client")]
@@ -26,69 +24,74 @@ struct Opt {
 async fn main() -> Result<()> {
     tracing::subscriber::set_global_default(
         tracing_subscriber::FmtSubscriber::builder()
-            .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+            .with_env_filter(
+                tracing_subscriber::EnvFilter::from_default_env()
+                    .add_directive(LevelFilter::INFO.into()),
+            )
             .finish(),
-    )
-    .unwrap();
-    let options = Opt::from_args();
+    )?;
+    let Opt { uri, ca } = Opt::from_args();
+    let socket_addr = (uri.host().unwrap(), uri.port_u16().unwrap_or(4433))
+        .to_socket_addrs()?
+        .next()
+        .expect("socket addr");
 
-    let mut client = ClientBuilder::default();
-    if let Some(ca_path) = options.ca {
-        client.add_certificate_authority(quinn::Certificate::from_der(&fs::read(&ca_path)?)?)?;
-    } else {
-        let dirs = directories::ProjectDirs::from("org", "quinn", "quinn-examples").unwrap();
-        match fs::read(dirs.data_local_dir().join("cert.der")) {
-            Ok(cert) => {
-                client.add_certificate_authority(quinn::Certificate::from_der(&cert)?)?;
-            }
-            Err(ref e) if e.kind() == io::ErrorKind::NotFound => {
-                info!("local server certificate not found");
-            }
-            Err(e) => {
-                error!("failed to open local server certificate: {}", e);
-            }
-        }
+    // Configure the client and build it
+    let mut client_builder = client::Builder::default();
+    if let Some(cert) = read_cert(&ca) {
+        client_builder.add_certificate_authority(cert)?;
+    }
+    let mut client = client_builder.build()?;
+
+    // Connect and wait for handshake completion
+    let conn = client
+        .connect(&socket_addr, uri.host().unwrap_or("localhost"))?
+        .await?;
+
+    let request = Request::get(uri)
+        .header("client", "quinn-h3:0.0.1")
+        .body(())?;
+
+    // Send the request
+    let (recv_response, _) = conn.send_request(request).await?;
+    // Wait for the response
+    let (response, mut recv_body) = recv_response.await?;
+
+    info!("received response: {:?}", response);
+
+    // Stream the response body into a vec
+    let mut body = Vec::with_capacity(1024);
+    recv_body.read_to_end(&mut body).await?;
+
+    info!("received body: {}", String::from_utf8_lossy(&body));
+
+    // Get the trailers if any
+    if let Some(trailers) = recv_body.trailers().await {
+        info!("received trailers: {:?}", trailers);
     }
 
-    let client = client.build()?;
-
-    match request(client, &options.uri).await {
-        Ok(_) => println!("client finished"),
-        Err(e) => println!("client failed: {:?}", e),
-    }
+    // Send a connection_close(NO_ERROR) to the server
+    conn.close();
+    // Make sure the server receives the closure frame
+    client.wait_idle().await;
 
     Ok(())
 }
 
-async fn request(client: Client, uri: &Uri) -> Result<()> {
-    let remote = (uri.host().unwrap(), uri.port_u16().unwrap_or(4433))
-        .to_socket_addrs()?
-        .next()
-        .ok_or_else(|| anyhow!("couldn't resolve to an address"))?;
-    let conn = client
-        .connect(&remote, uri.host().unwrap_or("localhost"))?
-        .await
-        .map_err(|e| anyhow!("failed ot connect: {:?}", e))?;
-
-    let request = Request::get(uri)
-        .header("client", "quinn-h3:0.0.1")
-        .body(())
-        .expect("failed to build request");
-
-    let (recv_response, _) = conn.send_request(request).await?;
-    let (response, mut recv_body) = recv_response.await?;
-
-    println!("received response: {:?}", response);
-
-    let mut body = Vec::with_capacity(1024);
-    recv_body.read_to_end(&mut body).await?;
-
-    println!("received body: {}", String::from_utf8_lossy(&body));
-
-    if let Some(trailers) = recv_body.trailers().await {
-        println!("received trailers: {:?}", trailers);
+fn read_cert(ca: &Option<PathBuf>) -> Option<quinn::Certificate> {
+    if let Some(ca_path) = ca {
+        return Some(quinn::Certificate::from_der(&fs::read(&ca_path).ok()?).ok()?);
     }
-    conn.close();
 
-    Ok(())
+    let dirs = directories::ProjectDirs::from("org", "quinn", "quinn-examples").unwrap();
+    match fs::read(dirs.data_local_dir().join("cert.der")) {
+        Ok(cert) => return Some(quinn::Certificate::from_der(&cert).ok()?),
+        Err(ref e) if e.kind() == io::ErrorKind::NotFound => {
+            info!("local server certificate not found");
+        }
+        Err(e) => {
+            error!("failed to open local server certificate: {}", e);
+        }
+    }
+    None
 }
