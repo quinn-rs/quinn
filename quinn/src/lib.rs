35db51eb1af0b26e4beb92a503aabb238c92a52c
Imbris <imbrisf@gmail.com>
2020-08-22 23:56:13 -0400

    Reexport ReadUnordered

--- quinn/src/lib.rs
+++ quinn/src/lib.rs
@@ -75,7 +75,7 @@ pub mod generic {
         OpenBi, OpenUni,
     };
     pub use crate::endpoint::{Endpoint, Incoming};
-    pub use crate::streams::{Read, ReadExact, ReadToEnd, RecvStream, SendStream};
+    pub use crate::streams::{Read, ReadExact, ReadToEnd, ReadUnordered, RecvStream, SendStream};
     pub use proto::generic::{ClientConfig, ServerConfig};
 }
 
@@ -124,6 +124,8 @@ mod rustls_impls {
     pub type ReadExact<'a> = generic::ReadExact<'a, TlsSession>;
     /// A `ReadToEnd` using rustls for the cryptography protocol
     pub type ReadToEnd = generic::ReadToEnd<TlsSession>;
+    /// A `ReadUnordered` using rustls for the cryptography protocol
+    pub type ReadUnordered<'a> = generic::ReadUnordered<'a, TlsSession>;
     /// A `RecvStream` using rustls for the cryptography protocol
     pub type RecvStream = generic::RecvStream<TlsSession>;
     /// A `SendStream` using rustls for the cryptography protocol
