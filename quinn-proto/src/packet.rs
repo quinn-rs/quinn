7065707d28915b181bfbd2584a0d0c48de90265c
jafow <jared.a.fowler@gmail.com>
2020-10-25 12:56:17 -0700

    changes PacketDecode visibility for fuzzing

--- quinn-proto/src/packet.rs
+++ quinn-proto/src/packet.rs
@@ -19,13 +19,13 @@ use crate::{
 // to inspect the version and packet type (which depends on the version).
 // This information allows us to fully decode and decrypt the packet.
 #[derive(Debug)]
-pub(crate) struct PartialDecode {
+pub struct PartialDecode {
     plain_header: PlainHeader,
     buf: io::Cursor<BytesMut>,
 }
 
 impl PartialDecode {
-    pub(crate) fn new(
+    pub fn new(
         bytes: BytesMut,
         local_cid_len: usize,
     ) -> Result<(Self, Option<BytesMut>), PacketDecodeError> {
@@ -710,7 +710,7 @@ pub(crate) enum LongType {
 }
 
 #[derive(Debug, Error, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
-pub(crate) enum PacketDecodeError {
+pub enum PacketDecodeError {
     #[error("unsupported version {version:x}")]
     UnsupportedVersion {
         src_cid: ConnectionId,
