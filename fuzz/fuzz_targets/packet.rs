df23acbc6839db63388f2d853d326cba722bd22c
jafow <jared.a.fowler@gmail.com>
2020-11-02 22:17:50 -0800

    fix length check for case when some data is returned

--- fuzz/fuzz_targets/packet.rs
+++ fuzz/fuzz_targets/packet.rs
@@ -7,6 +7,9 @@ use proto::fuzzing::{PacketParams, PartialDecode};
 fuzz_target!(|data: PacketParams| {
     let len = data.buf.len();
     if let Ok(decoded) = PartialDecode::new(data.buf, data.local_cid_len) {
-        assert_eq!(len, decoded.0.len());
+        match decoded.1 {
+            Some(x) => assert_eq!(len, decoded.0.len() + x.len()),
+            None => assert_eq!(len, decoded.0.len())
+        }
     }
 });
