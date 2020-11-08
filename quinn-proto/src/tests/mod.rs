8137e1bd52438ada595c78573627033ec2712825
Wenjie Li <liwenjie.sz@gmail.com>
2020-11-08 00:08:08 -0800

    Fix more fmt issue

--- quinn-proto/src/tests/mod.rs
+++ quinn-proto/src/tests/mod.rs
@@ -1239,7 +1239,8 @@ fn cid_retirement() {
     let (client_ch, server_ch) = pair.connect();
 
     // Server retires current active remote CIDs
-    pair.server_conn_mut(server_ch).rotate_local_cid(1, Instant::now());
+    pair.server_conn_mut(server_ch)
+        .rotate_local_cid(1, Instant::now());
     pair.drive();
     // Any unexpected behavior may trigger TransportError::CONNECTION_ID_LIMIT_ERROR
     assert!(!pair.client_conn_mut(client_ch).is_closed());
@@ -1254,7 +1255,8 @@ fn cid_retirement() {
     let next_retire_prior_to = active_cid_num + 1;
     pair.client_conn_mut(client_ch).ping();
     // Server retires all valid remote CIDs
-    pair.server_conn_mut(server_ch).rotate_local_cid(next_retire_prior_to, Instant::now());
+    pair.server_conn_mut(server_ch)
+        .rotate_local_cid(next_retire_prior_to, Instant::now());
     pair.drive();
     assert!(!pair.client_conn_mut(client_ch).is_closed());
     assert!(!pair.server_conn_mut(server_ch).is_closed());
