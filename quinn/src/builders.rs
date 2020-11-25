1928cc9b878bcccb97c25bc9c61133570062a224
liwenjieQu <liwenjie.sz@gmail.com>
2020-11-24 16:32:40 -0800

    Fix format issue after api change

--- quinn/src/builders.rs
+++ quinn/src/builders.rs
@@ -105,7 +105,9 @@ where
     }
 
     /// Use a customized cid generator factory in the endpoint
-    pub fn connection_id_generator<F: Fn() -> Box<dyn ConnectionIdGenerator> + Send + Sync + 'static>(
+    pub fn connection_id_generator<
+        F: Fn() -> Box<dyn ConnectionIdGenerator> + Send + Sync + 'static,
+    >(
         &mut self,
         factory: F,
     ) -> &mut Self {
