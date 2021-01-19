3c1a211bb0b35902d344fb51a0d1194f5a5fae30
Matthias Einwag <matthias.einwag@live.com>
2021-01-18 16:21:36 -0800

    Non contiguous Send buffer
    During testing with a multithreaded runtime I discovered that threads
    block a fair amount of time on Mutexes, even though everything in the
    state machine should be non-blocking.
    
    By inserting some more timing checks I discovered that the `poll_write`
    call took more than 10ms and locked the Mutex for this duration. This
    happened due to the `BytesMut::extend_from_slice` taking this time.
    
    The reason here is that extending the buffer require a reallocation of
    the buffer and a copy of the complete data. This can be a big size
    (up the maximum internal buffer size) - even if only a tiny chunk of
    data is added.
    
    This change improves on this by using a non-contiguous send buffer.
    New data is appended as a new `Bytes` segment, which only requires
    an allocation for this particular size. The 10ms blocking problem is
    gone with this.
    
    Another benefit of this change is that it can easily enable zero-copy
    writes by accepting `Bytes` as a parameter in the user-facing API.
    This is however not performed here yet.
    
    ```
    Jan 18 11:14:00.064  WARN quinn_proto::connection::send_buffer: self.unacked.extend_from_slice(data[..1048576]) took 10.3452ms. Now unacked size: 94924462
    Jan 18 11:14:00.064  WARN quinn_proto::connection::streams: Long Send::write. Total: 10.3943ms, Budget check: 0ns, data_len: 1048576, written: 1048576
    Jan 18 11:14:00.064  WARN quinn_proto::connection::streams: Long Streams::write. Total: 10.4203ms, Limit check: 100ns, Unsent check: 200ns, Write: 10.4202ms. data_len: 1048576, written: 1048576
    Jan 18 11:14:00.064  WARN quinn::streams: Long poll_write time: close_check 100ns, write_time 10.4456ms, end_time 10.4456ms
    ```

--- quinn-proto/src/connection/streams.rs
+++ quinn-proto/src/connection/streams.rs
@@ -609,7 +609,16 @@ impl Streams {
             let meta = frame::StreamMeta { id, offsets, fin };
             trace!(id = %meta.id, off = meta.offsets.start, len = meta.offsets.end - meta.offsets.start, fin = meta.fin, "STREAM");
             meta.encode(true, buf);
-            buf.put_slice(stream.pending.get(meta.offsets.clone()));
+
+            // The range might not be retrievable in a single `get` if it is
+            // stored in noncontiguous fashion. Therefore this loop iterates
+            // until the range is fully copied into the frame.
+            let mut offsets = meta.offsets.clone();
+            while offsets.start != offsets.end {
+                let data = stream.pending.get(offsets.clone());
+                offsets.start += data.len() as u64;
+                buf.put_slice(data);
+            }
             stream_frames.push(meta);
         }
 
