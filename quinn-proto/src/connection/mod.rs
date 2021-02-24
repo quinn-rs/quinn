quinn-rs/quinn

commit 095f402a9ff6539620a26a6d5fc44c71901a9d22

quinn-proto: inline single-use method

quinn-proto/src/connection/mod.rs
@@ -870,31 +870,40 @@ where
         let ack_eliciting = builder.ack_eliciting;
         let exact_number = builder.exact_number;
         let space_id = builder.space;
-        let (size, padded) = self.finish_packet(builder, buffer);
-
-        let mut sent = match sent {
+        let (size, _) = self.finish_packet(builder, buffer);
+        let sent = match sent {
             Some(sent) => sent,
             None => return,
         };
 
-        sent.padding = padded;
-        self.on_packet_sent(
-            now,
-            space_id,
-            exact_number,
-            SentPacket {
-                acks: sent.acks,
-                time_sent: now,
-                size: if sent.padding || ack_eliciting {
-                    size as u16
-                } else {
-                    0
-                },
-                ack_eliciting,
-                retransmits: sent.retransmits,
-                stream_frames: sent.stream_frames,
-            },
-        );
+        let size = match sent.padding || ack_eliciting {
+            true => size as u16,
+            false => 0,
+        };
+
+        let packet = SentPacket {
+            acks: sent.acks,
+            time_sent: now,
+            size,
+            ack_eliciting,
+            retransmits: sent.retransmits,
+            stream_frames: sent.stream_frames,
+        };
+
+        self.in_flight.insert(&packet);
+        self.spaces[space_id].sent(exact_number, packet);
+        self.reset_keep_alive(now);
+        if size != 0 {
+            if ack_eliciting {
+                self.spaces[space_id].time_of_last_ack_eliciting_packet = Some(now);
+                if self.permit_idle_reset {
+                    self.reset_idle_timeout(now);
+                }
+                self.permit_idle_reset = false;
+            }
+            self.set_loss_detection_timer(now);
+            self.path.pacing.on_transmit(size);
+        }
     }
 
     /// Process `ConnectionEvent`s generated by the associated `Endpoint`
@@ -1324,35 +1333,6 @@ where
     /// Current best estimate of this connection's latency (round-trip-time)
     pub fn rtt(&self) -> Duration {
         self.path.rtt.get()
-    }
-
-    fn on_packet_sent(
-        &mut self,
-        now: Instant,
-        space: SpaceId,
-        packet_number: u64,
-        packet: SentPacket,
-    ) {
-        let SentPacket {
-            size,
-            ack_eliciting,
-            ..
-        } = packet;
-
-        self.in_flight.insert(&packet);
-        self.spaces[space].sent(packet_number, packet);
-        self.reset_keep_alive(now);
-        if size != 0 {
-            if ack_eliciting {
-                self.spaces[space].time_of_last_ack_eliciting_packet = Some(now);
-                if self.permit_idle_reset {
-                    self.reset_idle_timeout(now);
-                }
-                self.permit_idle_reset = false;
-            }
-            self.set_loss_detection_timer(now);
-            self.path.pacing.on_transmit(size);
-        }
     }
 
     fn on_ack_received(

commit a7cee631b16b07f14b61367f3031b032854a9e8a

Fix regression in application of padding to congestion control
These was broken in 095f402a9ff6539620a26a6d5fc44c71901a9d22, and
rendered needlessly complex by
f1628f87a2d9313fbb0954b09cd10e884e64fa57 before it.

quinn-proto/src/connection/mod.rs
@@ -865,13 +865,13 @@ where
         let ack_eliciting = builder.ack_eliciting;
         let exact_number = builder.exact_number;
         let space_id = builder.space;
-        let (size, _) = self.finish_packet(builder, buffer);
+        let (size, padded) = self.finish_packet(builder, buffer);
         let sent = match sent {
             Some(sent) => sent,
             None => return,
         };
 
-        let size = match sent.padding || ack_eliciting {
+        let size = match padded || ack_eliciting {
             true => size as u16,
             false => 0,
         };
@@ -3613,7 +3613,6 @@ struct SentFrames {
     retransmits: ThinRetransmits,
     acks: RangeSet,
     stream_frames: StreamMetaVec,
-    padding: bool,
     requires_padding: bool,
 }
 