6a7c59f523af8070b636c2e81086db772081e859
Dirkjan Ochtman <dirkjan@ochtman.nl>
2021-02-24 11:47:12 +0100

    quinn-proto: move finish_packet() into PacketBuilder

--- quinn-proto/src/connection/packet_builder.rs
+++ quinn-proto/src/connection/packet_builder.rs
@@ -1,4 +1,10 @@
-use crate::packet::{PartialEncode, SpaceId};
+use tracing::trace;
+
+use crate::{
+    crypto::{PacketKey, Session},
+    generic::Connection,
+    packet::{PartialEncode, SpaceId},
+};
 
 pub(super) struct PacketBuilder {
     pub datagram_start: usize,
@@ -18,5 +24,48 @@ impl PacketBuilder {
         let prev = self.min_size;
         self.min_size = self.datagram_start + (min_size as usize) - self.tag_len;
         debug_assert!(self.min_size >= prev, "padding must not shrink datagram");
+    }
+
+    /// Encrypt packet, returning the length of the packet and whether padding was added
+    pub fn finish<S: Session>(
+        self: PacketBuilder,
+        conn: &mut Connection<S>,
+        buffer: &mut Vec<u8>,
+    ) -> (usize, bool) {
+        let pad = buffer.len() < self.min_size;
+        if pad {
+            trace!("PADDING * {}", self.min_size - buffer.len());
+            buffer.resize(self.min_size, 0);
+        }
+
+        let space = &conn.spaces[self.space];
+        let (header_crypto, packet_crypto) = if let Some(ref crypto) = space.crypto {
+            (&crypto.header.local, &crypto.packet.local)
+        } else if self.space == SpaceId::Data {
+            let zero_rtt = conn.zero_rtt_crypto.as_ref().unwrap();
+            (&zero_rtt.header, &zero_rtt.packet)
+        } else {
+            unreachable!("tried to send {:?} packet without keys", self.space);
+        };
+
+        debug_assert_eq!(
+            packet_crypto.tag_len(),
+            self.tag_len,
+            "Mismatching crypto tag len"
+        );
+
+        buffer.resize(buffer.len() + packet_crypto.tag_len(), 0);
+        debug_assert!(buffer.len() <= self.datagram_start + conn.path.mtu as usize);
+        let encode_start = self.partial_encode.start;
+        let packet_buf = &mut buffer[encode_start..];
+        self.partial_encode.finish(
+            packet_buf,
+            header_crypto,
+            Some((self.exact_number, packet_crypto)),
+        );
+        self.span
+            .with_subscriber(|(id, dispatch)| dispatch.exit(id));
+
+        (buffer.len() - encode_start, pad)
     }
 }
