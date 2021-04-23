cec86632593ab5de43e757b2aa31d7b861a2a6f9
David Craven <david@craven.ch>
2021-04-23 22:57:55 +0200

    Run rustfmt.

--- fuzz/fuzz_targets/packet.rs
+++ fuzz/fuzz_targets/packet.rs
@@ -11,9 +11,7 @@ use proto::{
 fuzz_target!(|data: PacketParams| {
     let len = data.buf.len();
     let supported_versions: Vec<_> = DEFAULT_SUPPORTED_VERSIONS.into_iter().collect();
-    if let Ok(decoded) =
-        PartialDecode::new(data.buf, data.local_cid_len, &supported_versions)
-    {
+    if let Ok(decoded) = PartialDecode::new(data.buf, data.local_cid_len, &supported_versions) {
         match decoded.1 {
             Some(x) => assert_eq!(len, decoded.0.len() + x.len()),
             None => assert_eq!(len, decoded.0.len()),
