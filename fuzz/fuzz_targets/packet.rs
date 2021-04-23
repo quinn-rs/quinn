edb15fb0c87e3105c26852a3cfa9323a04d4216d
David Craven <david@craven.ch>
2021-04-23 22:54:26 +0200

    Fix fuzzing.

--- fuzz/fuzz_targets/packet.rs
+++ fuzz/fuzz_targets/packet.rs
@@ -10,8 +10,9 @@ use proto::{
 
 fuzz_target!(|data: PacketParams| {
     let len = data.buf.len();
+    let supported_versions: Vec<_> = DEFAULT_SUPPORTED_VERSIONS.into_iter().collect();
     if let Ok(decoded) =
-        PartialDecode::new(data.buf, data.local_cid_len, DEFAULT_SUPPORTED_VERSIONS)
+        PartialDecode::new(data.buf, data.local_cid_len, &supported_versions)
     {
         match decoded.1 {
             Some(x) => assert_eq!(len, decoded.0.len() + x.len()),
