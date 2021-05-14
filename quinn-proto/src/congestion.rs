7c04a155c8296f5d7450033042fdc4e475ec6c08
Frank Spitulski <fspitulski@nvidia.com>
2021-05-14 16:19:58 -0700

    BBR Congestion Control
    squashed messages:
    
    fixup for docs and removing extra braces
    
    add downcasting hatch
    
    pretty print should be Display, not Debug
    
    un-needed export
    
    fmt and clippy
    
    re-expose CCAs to public API
    
    reorder constants
    
    inline bbr_state
    
    remove extra param
    
    refactor bbr into its own module
    
    refactor bbr into its own module
    
    no threadlocal rng, use genrange
    
    rename fields for review
    
    Update quinn/src/connection.rs
    
    Co-authored-by: Benjamin Saunders <ben.e.saunders@gmail.com>
    
    Update quinn-proto/src/connection/mod.rs
    
    Co-authored-by: Benjamin Saunders <ben.e.saunders@gmail.com>
    
    Update quinn-proto/src/connection/mod.rs
    
    Co-authored-by: Benjamin Saunders <ben.e.saunders@gmail.com>
    
    fill() now supported
    
    clippy in cubic was covered by the rebase
    
    add links to BBR info
    
    refactor to hide internal PathData
    
    clarify BBR usage
    
    reduce struct scope
    
    collapse update_last_sent into on_sent
    
    collapse on_loss into on_congestion_event
    
    remove dead code
    
    Update quinn/src/connection.rs
    
    Co-authored-by: Benjamin Saunders <ben.e.saunders@gmail.com>
    
    Update quinn-proto/src/congestion/bbr.rs
    
    Co-authored-by: Benjamin Saunders <ben.e.saunders@gmail.com>
    
    Update quinn-proto/src/congestion.rs
    
    Co-authored-by: Benjamin Saunders <ben.e.saunders@gmail.com>
    
    Update quinn-proto/src/congestion/cubic.rs
    
    Co-authored-by: Benjamin Saunders <ben.e.saunders@gmail.com>
    
    clippy
    
    safer duration comparison
    
    backwards compatibility fill
    
    add docs, rename stats cca to congestion
    
    remove PacketNumber and bbr_ prefixes
    
    rename constants to rust style
    
    fix packet number typing
    
    enable drain to target bdp mode during probe bw
    
    add ack aggregation for better target growth
    
    fix not entering growth mode
    
    remove extra debug printing
    
    enable rtt probe and track min rtt
    
    track max bandwidth with minmax filter
    
    use minmax filter for max bandwidth estimation
    
    switch minmax filter to use round counts instead of times
    
    port minmax filter to rust
    
    fix: count lost packet size instead of their id
    
    fix bw estimation not updating current bw
    
    expose congestion configs
    
    split bytes acked from end ack method
    
    simplify full bandwidth check
    
    remove some unused variables
    
    add 125% of target full bandwidth condition as per docs
    
    fix loss resetting too early, fix bandwidth target units of measurement
    
    check if full bw reached
    
    calculate recovery window
    
    calculate cwnd
    
    calculate pacing rate
    
    maybe_enter_or_exit_probe_rtt
    
    update gain cycle phase
    
    refactor bandwidth estimation
    
    add cca into stats for debugging
    
    add target window
    
    add recovery state transitions
    
    loss recording
    
    bandwidth estimation

--- quinn-proto/src/congestion.rs
+++ quinn-proto/src/congestion.rs
@@ -1,27 +1,62 @@
 //! Logic for controlling the rate at which data is sent
 
-use std::time::{Duration, Instant};
+use crate::connection::paths::RttEstimator;
+use std::any::Any;
+use std::time::Instant;
 
+mod bbr;
 mod cubic;
 mod new_reno;
 
+pub use bbr::{Bbr, BbrConfig};
 pub use cubic::{Cubic, CubicConfig};
 pub use new_reno::{NewReno, NewRenoConfig};
 
 /// Common interface for different congestion controllers
 pub trait Controller: Send {
+    /// One or more packets were just sent
+    #[allow(unused_variables)]
+    fn on_sent(&mut self, now: Instant, bytes: u64, last_packet_number: u64) {}
+
     /// Packet deliveries were confirmed
     ///
     /// `app_limited` indicates whether the connection was blocked on outgoing
     /// application data prior to receiving these acknowledgements.
-    fn on_ack(&mut self, now: Instant, sent: Instant, bytes: u64, app_limited: bool, rtt: Duration);
+    #[allow(unused_variables)]
+    fn on_ack(
+        &mut self,
+        now: Instant,
+        sent: Instant,
+        bytes: u64,
+        app_limited: bool,
+        rtt: &RttEstimator,
+    ) {
+    }
+
+    /// Packets are acked in batches, all with the same `now` argument. This indicates one of those batches has completed.
+    #[allow(unused_variables)]
+    fn on_end_acks(
+        &mut self,
+        now: Instant,
+        in_flight: u64,
+        app_limited: bool,
+        largest_packet_num_acked: Option<u64>,
+    ) {
+    }
 
     /// Packets were deemed lost or marked congested
     ///
     /// `in_persistent_congestion` indicates whether all packets sent within the persistent
     /// congestion threshold period ending when the most recent packet in this batch was sent were
     /// lost.
-    fn on_congestion_event(&mut self, now: Instant, sent: Instant, is_persistent_congestion: bool);
+    /// `lost_bytes` indicates how many bytes were lost. This value will be 0 for ECN triggers.
+    fn on_congestion_event(
+        &mut self,
+        now: Instant,
+        sent: Instant,
+        is_persistent_congestion: bool,
+        lost_bytes: u64,
+    );
 
     /// Number of ack-eliciting bytes that may be in flight
     fn window(&self) -> u64;
@@ -31,6 +66,9 @@ pub trait Controller: Send {
 
     /// Initial congestion window
     fn initial_window(&self) -> u64;
+
+    /// Returns Self for use in down-casting to extract implementation details
+    fn into_any(self: Box<Self>) -> Box<dyn Any>;
 }
 
 /// Constructs controllers on demand
--- quinn-proto/src/congestion/new_reno.rs
+++ quinn-proto/src/congestion/new_reno.rs
@@ -1,7 +1,9 @@
+use std::any::Any;
 use std::sync::Arc;
-use std::time::{Duration, Instant};
+use std::time::Instant;
 
 use super::{Controller, ControllerFactory};
+use crate::connection::paths::RttEstimator;
 
 /// A simple, standard congestion controller
 #[derive(Debug, Clone)]
@@ -39,7 +41,7 @@ impl Controller for NewReno {
         sent: Instant,
         bytes: u64,
         app_limited: bool,
-        _rtt: Duration,
+        _rtt: &RttEstimator,
     ) {
         if app_limited || sent <= self.recovery_start_time {
             return;
@@ -74,7 +76,13 @@ impl Controller for NewReno {
         }
     }
 
-    fn on_congestion_event(&mut self, now: Instant, sent: Instant, is_persistent_congestion: bool) {
+    fn on_congestion_event(
+        &mut self,
+        now: Instant,
+        sent: Instant,
+        is_persistent_congestion: bool,
+        _lost_bytes: u64,
+    ) {
         if sent <= self.recovery_start_time {
             return;
         }
@@ -99,6 +107,10 @@ impl Controller for NewReno {
 
     fn initial_window(&self) -> u64 {
         self.config.initial_window
+    }
+
+    fn into_any(self: Box<Self>) -> Box<dyn Any> {
+        self
     }
 }
 
