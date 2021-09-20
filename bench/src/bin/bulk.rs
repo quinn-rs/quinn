a28b809d3e1de50c8d7fa659bcd6bbfa39cb430d
Matthias Einwag <matthias.einwag@live.com>
2021-09-19 19:28:07 -0700

    Allow multiple clients in the bulk benchmark
    This allows to specify a "--client" parameter in the benchmark,
    which specifies the amount of clients which perform a  test concurrently.
    Each client uses its own thread and single-threaded runtime.
    
    Stats are currently printed for each individual client and not aggregated.

--- bench/src/bin/bulk.rs
+++ bench/src/bin/bulk.rs
@@ -23,57 +23,93 @@ fn main() {
     let runtime = rt();
     let (server_addr, incoming) = server_endpoint(&runtime, cert.clone(), key, &opt);
 
-    let thread = std::thread::spawn(move || {
+    let server_thread = std::thread::spawn(move || {
         if let Err(e) = runtime.block_on(server(incoming, opt)) {
             eprintln!("server failed: {:#}", e);
         }
     });
 
-    let runtime = rt();
-    if let Err(e) = runtime.block_on(client(server_addr, cert, opt)) {
-        eprintln!("client failed: {:#}", e);
-    }
-
-    thread.join().expect("server thread");
-}
-
-async fn server(mut incoming: quinn::Incoming, opt: Opt) -> Result<()> {
-    let handshake = incoming.next().await.unwrap();
-    let quinn::NewConnection {
-        mut bi_streams,
-        connection,
-        ..
-    } = handshake.await.context("handshake failed")?;
-
-    let mut result = Ok(());
-
-    loop {
-        let (mut send_stream, mut recv_stream) = match bi_streams.next().await {
-            None => break,
-            Some(Err(quinn::ConnectionError::ApplicationClosed(_))) => break,
-            Some(Err(e)) => {
-                result = Err(e).context("accepting stream failed");
-                break;
+    let mut handles = Vec::new();
+    for _ in 0..opt.clients {
+        let cert = cert.clone();
+        handles.push(std::thread::spawn(move || {
+            let runtime = rt();
+            match runtime.block_on(client(server_addr, cert, opt)) {
+                Ok(stats) => Ok(stats),
+                Err(e) => {
+                    eprintln!("client failed: {:#}", e);
+                    Err(e)
+                }
             }
-            Some(Ok(stream)) => stream,
-        };
-        trace!("stream established");
-
-        let _: tokio::task::JoinHandle<Result<()>> = tokio::spawn(async move {
-            drain_stream(&mut recv_stream, opt.read_unordered).await?;
-            send_data_on_stream(&mut send_stream, opt.download_size).await?;
-            Ok(())
-        });
+        }));
     }
 
-    if opt.stats {
-        println!("\nServer connection stats:\n{:#?}", connection.stats());
+    for (id, handle) in handles.into_iter().enumerate() {
+        // We print all stats at the end of the test sequentially to avoid
+        // them being garbled due to being printed concurrently
+        if let Ok(stats) = handle.join().expect("client thread") {
+            stats.print(id);
+        }
     }
 
-    result
+    server_thread.join().expect("server thread");
 }
 
-async fn client(server_addr: SocketAddr, server_cert: quinn::Certificate, opt: Opt) -> Result<()> {
+async fn server(incoming: quinn::Incoming, opt: Opt) -> Result<()> {
+    // Handle only the expected amount of clients
+    let mut incoming = incoming.take(opt.clients);
+
+    let mut server_tasks = Vec::new();
+
+    while let Some(handshake) = incoming.next().await {
+        let quinn::NewConnection {
+            mut bi_streams,
+            connection,
+            ..
+        } = handshake.await.context("handshake failed")?;
+
+        server_tasks.push(tokio::spawn(async move {
+            loop {
+                let (mut send_stream, mut recv_stream) = match bi_streams.next().await {
+                    None => break,
+                    Some(Err(quinn::ConnectionError::ApplicationClosed(_))) => break,
+                    Some(Err(e)) => {
+                        eprintln!("accepting stream failed: {:?}", e);
+                        break;
+                    }
+                    Some(Ok(stream)) => stream,
+                };
+                trace!("stream established");
+
+                let _: tokio::task::JoinHandle<Result<()>> = tokio::spawn(async move {
+                    drain_stream(&mut recv_stream, opt.read_unordered).await?;
+                    send_data_on_stream(&mut send_stream, opt.download_size).await?;
+                    Ok(())
+                });
+            }
+
+            if opt.stats {
+                println!("\nServer connection stats:\n{:#?}", connection.stats());
+            }
+        }));
+    }
+
+    // Await all the tasks. We have to do this to prevent the runtime getting dropped
+    // and all server tasks to be cancelled
+    for handle in server_tasks {
+        if let Err(e) = handle.await {
+            eprintln!("Server task error: {:?}", e);
+        };
+    }
+
+    Ok(())
+}
+
+async fn client(
+    server_addr: SocketAddr,
+    server_cert: quinn::Certificate,
+    opt: Opt,
+) -> Result<ClientStats> {
     let (endpoint, connection) = connect_client(server_addr, server_cert, opt).await?;
 
     let start = Instant::now();
@@ -91,34 +127,26 @@ async fn client(server_addr: SocketAddr, server_cert: quinn::Certificate, opt: O
         )
         .buffer_unordered(opt.max_streams);
 
-    let mut upload_stats = Stats::default();
-    let mut download_stats = Stats::default();
-
-    let mut result = Ok(());
+    let mut stats = ClientStats::default();
+    let mut first_error = None;
 
     while let Some(stream_result) = ops.next().await {
         info!("stream finished: {:?}", stream_result);
         match stream_result {
             Ok((upload_result, download_result)) => {
-                upload_stats.stream_finished(upload_result);
-                download_stats.stream_finished(download_result);
+                stats.upload_stats.stream_finished(upload_result);
+                stats.download_stats.stream_finished(download_result);
             }
             Err(e) => {
-                if result.is_ok() {
-                    result = Err(e);
+                if first_error.is_none() {
+                    first_error = Some(e);
                 }
             }
         }
     }
 
-    upload_stats.total_duration = start.elapsed();
-    download_stats.total_duration = start.elapsed();
-    if upload_stats.total_size != 0 {
-        upload_stats.print("upload");
-    }
-    if download_stats.total_size != 0 {
-        download_stats.print("download");
-    }
+    stats.upload_stats.total_duration = start.elapsed();
+    stats.download_stats.total_duration = start.elapsed();
 
     // Explicit close of the connection, since handles can still be around due
     // to `Arc`ing them
@@ -130,7 +158,10 @@ async fn client(server_addr: SocketAddr, server_cert: quinn::Certificate, opt: O
         println!("\nClient connection stats:\n{:#?}", connection.stats());
     }
 
-    result
+    match first_error {
+        None => Ok(stats),
+        Some(e) => Err(e),
+    }
 }
 
 async fn handle_client_stream(
@@ -154,4 +185,25 @@ async fn handle_client_stream(
     let download_result = TransferResult::new(start.elapsed(), size);
 
     Ok((upload_result, download_result))
+}
+
+#[derive(Default)]
+struct ClientStats {
+    upload_stats: Stats,
+    download_stats: Stats,
+}
+
+impl ClientStats {
+    pub fn print(&self, client_id: usize) {
+        println!();
+        println!("Client {} stats:", client_id);
+
+        if self.upload_stats.total_size != 0 {
+            self.upload_stats.print("upload");
+        }
+
+        if self.download_stats.total_size != 0 {
+            self.download_stats.print("download");
+        }
+    }
 }
