2117e1fec584661a551940092baec3fa3f5625b7
Benjamin Saunders <ben.e.saunders@gmail.com>
2022-01-02 11:22:32 -0800

    Privatize VarInt::size
    This method isn't useful for converting between VarInts and regular
    integers, and hence serves no purpose in the public API.

--- quinn-proto/src/varint.rs
+++ quinn-proto/src/varint.rs
@@ -51,7 +51,7 @@ impl VarInt {
     }
 
     /// Compute the number of bytes needed to encode this value
-    pub fn size(self) -> usize {
+    pub(crate) fn size(self) -> usize {
         let x = self.0;
         if x < 2u64.pow(6) {
             1
