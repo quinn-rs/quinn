f02dfc2f2668e00911702f4cbe7e8ab074cc35c9
Adolfo Ochagavía <github@adolfo.ochagavia.nl>
2023-04-05 14:24:07 +0200

    Detect black holes based on loss bursts for robustness
    Sponsored by Stormshield

--- quinn-proto/src/connection/stats.rs
+++ quinn-proto/src/connection/stats.rs
@@ -137,6 +137,8 @@ pub struct PathStats {
     /// The amount of PLPMTUD probe packets lost on this path (ignored by `lost_packets` and
     /// `lost_bytes`)
     pub lost_plpmtud_probes: u64,
+    /// The number of times a black hole was detected in the path
+    pub black_holes_detected: u64,
 }
 
 /// Connection statistics
