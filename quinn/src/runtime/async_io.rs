2e4074afb155f3f1e58aff10391aa8b4819e3883
Al Liu <scygliu1@gmail.com>
2023-12-11 20:39:04 +0800

    Copy the AsyncStdRuntime module to create SmolRuntime

--- quinn/src/runtime/async_io.rs
+++ quinn/src/runtime/async_io.rs
@@ -11,6 +11,35 @@ use async_io::{Async, Timer};
 
 use super::{AsyncTimer, AsyncUdpSocket, Runtime};
 
+#[cfg(feature = "smol")]
+pub use self::smol::SmolRuntime;
+
+#[cfg(feature = "smol")]
+mod smol {
+    use super::*;
+
+    /// A Quinn runtime for smol
+    #[derive(Debug)]
+    pub struct SmolRuntime;
+
+    impl Runtime for SmolRuntime {
+        fn new_timer(&self, t: Instant) -> Pin<Box<dyn AsyncTimer>> {
+            Box::pin(Timer::at(t))
+        }
+
+        fn spawn(&self, future: Pin<Box<dyn Future<Output = ()> + Send>>) {
+            ::smol::spawn(future).detach();
+        }
+
+        fn wrap_udp_socket(
+            &self,
+            sock: std::net::UdpSocket,
+        ) -> io::Result<Arc<dyn AsyncUdpSocket>> {
+            Ok(Arc::new(UdpSocket::new(sock)?))
+        }
+    }
+}
+
 #[cfg(feature = "async-std")]
 pub use self::async_std::AsyncStdRuntime;
 
