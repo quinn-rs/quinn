14a3e93e596b332c0b2417b7c83f3d06df2bebf0
Benjamin Saunders <ben.e.saunders@gmail.com>
2024-02-18 17:16:58 -0800

    Remove redundant assert

--- quinn-proto/src/connection/mod.rs
+++ quinn-proto/src/connection/mod.rs
@@ -2974,7 +2974,6 @@ impl Connection {
 
         // ACK
         if space.pending_acks.can_send() {
-            debug_assert!(!space.pending_acks.ranges().is_empty());
             Self::populate_acks(
                 now,
                 self.receiving_ecn,
