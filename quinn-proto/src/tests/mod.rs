169f0c2b4380fa7fc4923d9a7a3058f4bc8939d2
Benjamin Saunders <ben.e.saunders@gmail.com>
2024-07-30 18:08:26 -0700

    Fix incorrect initial DCID indexing on retried connections
    This could also cause other packets received before the server accepts
    the connection to be lost.

--- quinn-proto/src/tests/mod.rs
+++ quinn-proto/src/tests/mod.rs
@@ -172,7 +172,17 @@ fn stateless_retry() {
     let _guard = subscribe();
     let mut pair = Pair::default();
     pair.server.incoming_connection_behavior = IncomingConnectionBehavior::Validate;
-    pair.connect();
+    let (client_ch, _server_ch) = pair.connect();
+    pair.client
+        .connections
+        .get_mut(&client_ch)
+        .unwrap()
+        .close(pair.time, VarInt(42), Bytes::new());
+    pair.drive();
+    assert_eq!(pair.client.known_connections(), 0);
+    assert_eq!(pair.client.known_cids(), 0);
+    assert_eq!(pair.server.known_connections(), 0);
+    assert_eq!(pair.server.known_cids(), 0);
 }
 
 #[test]
