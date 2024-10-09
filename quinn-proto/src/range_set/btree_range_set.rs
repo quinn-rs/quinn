5dd3497107e97b6341eb519f080fd13907f26855
Lars Eggert <lars@eggert.org>
2024-10-09 10:18:38 +0300

    chore: `cargo +nightly clippy --fix`

--- quinn-proto/src/range_set/btree_range_set.rs
+++ quinn-proto/src/range_set/btree_range_set.rs
@@ -212,7 +212,7 @@ impl RangeSet {
 
 pub struct Iter<'a>(btree_map::Iter<'a, u64, u64>);
 
-impl<'a> Iterator for Iter<'a> {
+impl Iterator for Iter<'_> {
     type Item = Range<u64>;
     fn next(&mut self) -> Option<Range<u64>> {
         let (&start, &end) = self.0.next()?;
@@ -220,7 +220,7 @@ impl<'a> Iterator for Iter<'a> {
     }
 }
 
-impl<'a> DoubleEndedIterator for Iter<'a> {
+impl DoubleEndedIterator for Iter<'_> {
     fn next_back(&mut self) -> Option<Range<u64>> {
         let (&start, &end) = self.0.next_back()?;
         Some(start..end)
@@ -241,7 +241,7 @@ pub struct EltIter<'a> {
     end: u64,
 }
 
-impl<'a> Iterator for EltIter<'a> {
+impl Iterator for EltIter<'_> {
     type Item = u64;
     fn next(&mut self) -> Option<u64> {
         if self.next == self.end {
@@ -255,7 +255,7 @@ impl<'a> Iterator for EltIter<'a> {
     }
 }
 
-impl<'a> DoubleEndedIterator for EltIter<'a> {
+impl DoubleEndedIterator for EltIter<'_> {
     fn next_back(&mut self) -> Option<u64> {
         if self.next == self.end {
             let (&start, &end) = self.inner.next_back()?;
