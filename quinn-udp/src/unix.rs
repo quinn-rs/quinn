bb4df4d35c84281ba25b7d5ac0e6e5f35279d94c
Delta 4 <156182500+delta4chat@users.noreply.github.com>
2024-11-12 19:02:13 +0800

    fixes https://github.com/quinn-rs/quinn/issues/2033

--- quinn-udp/src/unix.rs
+++ quinn-udp/src/unix.rs
@@ -445,7 +445,12 @@ fn send(state: &UdpSocketState, io: SockRef<'_>, transmit: &Transmit<'_>) -> io:
     Ok(())
 }
 
-#[cfg(not(any(apple, target_os = "openbsd", target_os = "solaris")))]
+#[cfg(not(any(
+    apple,
+    target_os = "openbsd",
+    target_os = "netbsd",
+    target_os = "solaris"
+)))]
 fn recv(io: SockRef<'_>, bufs: &mut [IoSliceMut<'_>], meta: &mut [RecvMeta]) -> io::Result<usize> {
     let mut names = [MaybeUninit::<libc::sockaddr_storage>::uninit(); BATCH_SIZE];
     let mut ctrls = [cmsg::Aligned(MaybeUninit::<[u8; CMSG_LEN]>::uninit()); BATCH_SIZE];
