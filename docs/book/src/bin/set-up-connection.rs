a196f7c48049c7e26ed51449f3ab3f0746e88ce7
Dirkjan Ochtman <dirkjan@ochtman.nl>
2025-03-30 17:11:30 +0200

    book: simplify connection setup constants

--- docs/book/src/bin/set-up-connection.rs
+++ docs/book/src/bin/set-up-connection.rs
@@ -1,21 +1,16 @@
 use quinn::{Endpoint, ServerConfig};
 use std::error::Error;
-use std::net::SocketAddr;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
-static SERVER_NAME: &str = "localhost";
-
-fn client_addr() -> SocketAddr {
-    "127.0.0.1:5000".parse::<SocketAddr>().unwrap()
-}
-
-fn server_addr() -> SocketAddr {
-    "127.0.0.1:5001".parse::<SocketAddr>().unwrap()
-}
+const SERVER_NAME: &str = "localhost";
+const LOCALHOST_V4: IpAddr = IpAddr::V4(Ipv4Addr::LOCALHOST);
+const CLIENT_ADDR: SocketAddr = SocketAddr::new(LOCALHOST_V4, 5000);
+const SERVER_ADDR: SocketAddr = SocketAddr::new(LOCALHOST_V4, 5001);
 
 #[allow(dead_code, unused_variables)] // Included in `set-up-connection.md`
 async fn server(config: ServerConfig) -> Result<(), Box<dyn Error>> {
     // Bind this endpoint to a UDP socket on the given server address.
-    let endpoint = Endpoint::server(config, server_addr())?;
+    let endpoint = Endpoint::server(config, SERVER_ADDR)?;
 
     // Start iterating over incoming connections.
     while let Some(conn) = endpoint.accept().await {
@@ -30,10 +25,10 @@ async fn server(config: ServerConfig) -> Result<(), Box<dyn Error>> {
 #[allow(dead_code, unused_variables)] // Included in `set-up-connection.md`
 async fn client() -> Result<(), Box<dyn Error>> {
     // Bind this endpoint to a UDP socket on the given client address.
-    let endpoint = Endpoint::client(client_addr())?;
+    let endpoint = Endpoint::client(CLIENT_ADDR)?;
 
     // Connect to the server passing in the server name which is supposed to be in the server certificate.
-    let connection = endpoint.connect(server_addr(), SERVER_NAME)?.await?;
+    let connection = endpoint.connect(SERVER_ADDR, SERVER_NAME)?.await?;
 
     // Start transferring, receiving data, see data transfer page.
 
