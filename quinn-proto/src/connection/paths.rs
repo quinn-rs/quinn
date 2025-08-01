67a0d2e4f173ea98196a51fe71b6ecb053897472
Benjamin Saunders <ben.e.saunders@gmail.com>
2025-07-31 17:15:20 -0700

    Introduce unique path identifier

--- quinn-proto/src/connection/paths.rs
+++ quinn-proto/src/connection/paths.rs
@@ -49,6 +49,9 @@ pub(super) struct PathData {
     /// Snapshot of the qlog recovery metrics
     #[cfg(feature = "qlog")]
     recovery_metrics: RecoveryMetrics,
+
+    /// Tag uniquely identifying a path in a connection
+    generation: u64,
 }
 
 impl PathData {
@@ -56,6 +59,7 @@ impl PathData {
         remote: SocketAddr,
         allow_mtud: bool,
         peer_max_udp_payload_size: Option<u16>,
+        generation: u64,
         now: Instant,
         config: &TransportConfig,
     ) -> Self {
@@ -99,10 +103,16 @@ impl PathData {
             first_packet: None,
             #[cfg(feature = "qlog")]
             recovery_metrics: RecoveryMetrics::default(),
+            generation,
         }
     }
 
-    pub(super) fn from_previous(remote: SocketAddr, prev: &Self, now: Instant) -> Self {
+    pub(super) fn from_previous(
+        remote: SocketAddr,
+        prev: &Self,
+        generation: u64,
+        now: Instant,
+    ) -> Self {
         let congestion = prev.congestion.clone_box();
         let smoothed_rtt = prev.rtt.get();
         Self {
@@ -122,6 +132,7 @@ impl PathData {
             first_packet: None,
             #[cfg(feature = "qlog")]
             recovery_metrics: prev.recovery_metrics.clone(),
+            generation,
         }
     }
 
@@ -188,6 +199,10 @@ impl PathData {
         let event = metrics.to_qlog_event(&self.recovery_metrics);
         self.recovery_metrics = metrics;
         event
+    }
+
+    pub(super) fn generation(&self) -> u64 {
+        self.generation
     }
 }
 
