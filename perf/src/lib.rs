3153ea7d1ef5a501260d84aa65f7cf65d303f46f
Damien Deville <damien.deville@stormshield.eu>
2025-08-14 11:25:40 +0200

    perf: factorize common options

--- perf/src/lib.rs
+++ perf/src/lib.rs
@@ -1,8 +1,13 @@
-use std::{net::SocketAddr, sync::Arc};
+#[cfg(feature = "qlog")]
+use std::{fs::File, path::PathBuf};
+use std::{io, net::SocketAddr, sync::Arc, time::Duration};
 
 use anyhow::{Context, Result};
-use clap::ValueEnum;
+use clap::{Parser, ValueEnum};
+#[cfg(feature = "qlog")]
+use quinn::QlogConfig;
 use quinn::{
+    AckFrequencyConfig, TransportConfig,
     congestion::{self, ControllerFactory},
     udp::UdpSocketState,
 };
@@ -14,6 +19,74 @@ use tracing::warn;
 pub mod stats;
 
 pub mod noprotection;
+
+// Common options between client and server binary
+#[derive(Parser)]
+pub struct CommonOpt {
+    /// Send buffer size in bytes
+    #[clap(long, default_value = "2097152")]
+    pub send_buffer_size: usize,
+    /// Receive buffer size in bytes
+    #[clap(long, default_value = "2097152")]
+    pub recv_buffer_size: usize,
+    /// Whether to print connection statistics
+    #[clap(long)]
+    pub conn_stats: bool,
+    /// Perform NSS-compatible TLS key logging to the file specified in `SSLKEYLOGFILE`.
+    #[clap(long = "keylog")]
+    pub keylog: bool,
+    /// UDP payload size that the network must be capable of carrying
+    #[clap(long, default_value = "1200")]
+    pub initial_mtu: u16,
+    /// Disable packet encryption/decryption (for debugging purpose)
+    #[clap(long = "no-protection")]
+    pub no_protection: bool,
+    /// The initial round-trip-time (in msecs)
+    #[clap(long, group = "common")]
+    pub initial_rtt: Option<u64>,
+    /// Ack Frequency mode
+    #[clap(long = "ack-frequency")]
+    pub ack_frequency: bool,
+    /// Congestion algorithm to use
+    #[clap(long = "congestion")]
+    pub cong_alg: Option<CongestionAlgorithm>,
+    /// qlog output file
+    #[cfg(feature = "qlog")]
+    #[clap(long = "qlog")]
+    pub qlog_file: Option<PathBuf>,
+}
+
+impl CommonOpt {
+    pub fn build_transport_config(
+        &self,
+        #[cfg(feature = "qlog")] name: &str,
+    ) -> io::Result<TransportConfig> {
+        let mut transport = TransportConfig::default();
+        transport.initial_mtu(self.initial_mtu);
+
+        if let Some(initial_rtt) = self.initial_rtt {
+            transport.initial_rtt(Duration::from_millis(initial_rtt));
+        }
+
+        if self.ack_frequency {
+            transport.ack_frequency_config(Some(AckFrequencyConfig::default()));
+        }
+
+        if let Some(cong_alg) = self.cong_alg {
+            transport.congestion_controller_factory(cong_alg.build());
+        }
+
+        #[cfg(feature = "qlog")]
+        if let Some(qlog_file) = &self.qlog_file {
+            let mut qlog = QlogConfig::default();
+            qlog.writer(Box::new(File::create(qlog_file)?))
+                .title(Some(name.into()));
+            transport.qlog_stream(qlog.into_stream());
+        }
+
+        Ok(transport)
+    }
+}
 
 #[derive(Clone, Copy, ValueEnum)]
 pub enum CongestionAlgorithm {
