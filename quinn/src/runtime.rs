0e2d551c27f8f613a874e067ca7f2e8b8d56c800
Dirkjan Ochtman <dirkjan@ochtman.nl>
2025-09-01 10:00:37 +0200

    Remove more traces of async-std

--- quinn/src/runtime.rs
+++ quinn/src/runtime.rs
@@ -234,9 +234,8 @@ pub trait UdpSenderHelperSocket: Send + Sync + 'static {
 /// Automatically select an appropriate runtime from those enabled at compile time
 ///
 /// If `runtime-tokio` is enabled and this function is called from within a Tokio runtime context,
-/// then `TokioRuntime` is returned. Otherwise, if `runtime-async-std` is enabled, `AsyncStdRuntime`
-/// is returned. Otherwise, if `runtime-smol` is enabled, `SmolRuntime` is returned.
-/// Otherwise, `None` is returned.
+/// then `TokioRuntime` is returned. Otherwise, if `runtime-smol` is enabled, `SmolRuntime` is
+/// returned. Otherwise, `None` is returned.
 #[allow(clippy::needless_return)] // Be sure we return the right thing
 pub fn default_runtime() -> Option<Arc<dyn Runtime>> {
     #[cfg(feature = "runtime-tokio")]
