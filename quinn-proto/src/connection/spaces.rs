6a984b8d5f27bac35af551f839df65eb8c06c113
Dirkjan Ochtman <dirkjan@ochtman.nl>
2026-07-23 10:46:11 +0200

    proto: extract method for checking CID retirements

--- quinn-proto/src/connection/spaces.rs
+++ quinn-proto/src/connection/spaces.rs
@@ -2,7 +2,7 @@ use std::{
     cmp,
     collections::{BTreeMap, VecDeque},
     mem,
-    ops::{Bound, Index, IndexMut},
+    ops::{Bound, Index, IndexMut, Range},
 };
 
 use rand::{Rng, RngExt};
@@ -11,8 +11,9 @@ use tracing::trace;
 
 use super::assembler::Assembler;
 use crate::{
-    Dir, Duration, Instant, SocketAddr, StreamId, TransportError, VarInt, connection::StreamsState,
-    crypto::Keys, frame, packet::SpaceId, range_set::ArrayRangeSet, shared::IssuedCid,
+    Dir, Duration, Instant, SocketAddr, StreamId, TransportError, VarInt, cid_queue::CidQueue,
+    connection::StreamsState, crypto::Keys, frame, packet::SpaceId, range_set::ArrayRangeSet,
+    shared::IssuedCid,
 };
 
 pub(super) struct PacketSpace {
@@ -336,6 +337,19 @@ pub struct Retransmits {
 }
 
 impl Retransmits {
+    pub(super) fn retire_cids(&mut self, cids: Range<u64>) -> Result<(), TransportError> {
+        // We don't bother counting in-flight frames because those are bounded by congestion control.
+        let num = cids.end.saturating_sub(cids.start);
+        if (self.retire_cids.len() as u64).saturating_add(num) > Self::MAX_PENDING_RETIRED_CIDS {
+            return Err(TransportError::CONNECTION_ID_LIMIT_ERROR(
+                "queued too many retired CIDs",
+            ));
+        }
+
+        self.retire_cids.extend(cids);
+        Ok(())
+    }
+
     pub(super) fn is_empty(&self, streams: &StreamsState) -> bool {
         !self.max_data
             && !self.max_stream_id.into_iter().any(|x| x)
@@ -352,6 +366,11 @@ impl Retransmits {
             && !self.handshake_done
             && self.new_tokens.is_empty()
     }
+
+    /// Ensure `pending_retired` cannot grow without bound
+    ///
+    /// Limit is somewhat arbitrary but very permissive.
+    const MAX_PENDING_RETIRED_CIDS: u64 = CidQueue::LEN as u64 * 10;
 }
 
 impl ::std::ops::BitOrAssign for Retransmits {
