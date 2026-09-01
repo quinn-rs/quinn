ec7987f83eae99c548a0e0b923368b864e885833
Phoenix K <kahlo.phoenix@gmail.com>
2026-08-31 20:20:20 -0500

    refactor(quinn): More defensive against underflow
    The fact that this skip calculation (`self.bytes_read - front.offset`)
    never underflows relies on the fact that each loop iteration
    short-circuits if (`front.offset > self.bytes_read`) before this
    statement is reached. Makes this more defensive by using `if let` and
    calculating `skip` via `checked_sub`.
    
    This does result in this `if` block running when `skip` is 0, whereas it
    previously does not. However, this does not affect program behavior, and
    should not affect performance.

--- quinn-proto/src/connection/assembler.rs
+++ quinn-proto/src/connection/assembler.rs
@@ -76,13 +76,12 @@ impl Assembler {
                     continue;
                 }
 
-                // Advance front to the slice of useful data
-                let skip = (self.bytes_read - front.offset) as usize;
-                // Advancing the offset can push the front past data[1]; both exits below fix that
-                if skip > 0 {
-                    front.bytes.advance(skip);
-                    front.offset += skip as u64;
-                    self.buffered -= skip;
+                if let Some(skip) = self.bytes_read.checked_sub(front.offset) {
+                    // Advance front to the slice of useful data
+                    // Advancing the offset can push the front past data[1]; both exits below fix that
+                    front.bytes.advance(skip as usize);
+                    front.offset += skip;
+                    self.buffered -= skip as usize;
                 }
             }
 
