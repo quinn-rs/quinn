a781310e16b0d8adf017d49186edc90818a778ab
zerox80 <rujbin@protonmail.com>
2026-09-07 02:36:49 +0200

    proto: extract oversized DATAGRAM cleanup
    Move MTU-related queue pruning and the blocked-sender notification into a connection helper without changing when it is called.

--- quinn-proto/src/connection/mod.rs
+++ quinn-proto/src/connection/mod.rs
@@ -1878,13 +1878,7 @@ impl Connection {
                 self.path
                     .congestion
                     .on_mtu_update(self.path.mtud.current_mtu());
-                if let Some(max_datagram_size) = self.datagrams().max_size()
-                    && self.datagrams.drop_oversized(max_datagram_size)
-                    && self.datagrams.send_blocked
-                {
-                    self.datagrams.send_blocked = false;
-                    self.events.push_back(Event::DatagramsUnblocked);
-                }
+                self.datagrams().drop_oversized();
             }
 
             // Don't apply congestion penalty for lost ack-only packets
